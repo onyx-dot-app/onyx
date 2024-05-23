c363708e2afc9722246cab67a20c20ead6f0a36a
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2024-05-23 10:57:46 +0300

    updated logo and title

--- web/src/app/chat/sessionSidebar/ChatSidebar.tsx
+++ web/src/app/chat/sessionSidebar/ChatSidebar.tsx
@@ -68,11 +68,11 @@ export const ChatSidebar = ({
             }
           >
             <div className="flex w-full">
-              <div className="h-[32px] w-[30px]">
-                <Image src="/logo.png" alt="Logo" width="1419" height="1520" />
+              <div className="h-[32px] w-[80px]">
+                <Image src="/EEA_logo_compact_EN.svg" alt="Logo" width="1419" height="1520" />
               </div>
               <h1 className="flex text-2xl text-strong font-bold my-auto">
-                Danswer
+                &nbsp;GPT Lab
               </h1>
             </div>
           </Link>
