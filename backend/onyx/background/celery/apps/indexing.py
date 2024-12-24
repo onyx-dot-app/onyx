1126a3b0c2e46ace565ff0c9de72fe471dd6ba74
Richard Kuo (Danswer) <rkuo@onyx.app>
2024-12-24 11:11:25 -0800

    raise concurrency overflow for indexing worker.

--- backend/onyx/background/celery/apps/indexing.py
+++ backend/onyx/background/celery/apps/indexing.py
@@ -60,7 +60,9 @@ def on_worker_init(sender: Any, **kwargs: Any) -> None:
     logger.info(f"Multiprocessing start method: {multiprocessing.get_start_method()}")
 
     SqlEngine.set_app_name(POSTGRES_CELERY_WORKER_INDEXING_APP_NAME)
-    SqlEngine.init_engine(pool_size=sender.concurrency, max_overflow=sender.concurrency)
+    SqlEngine.init_engine(
+        pool_size=sender.concurrency, max_overflow=sender.concurrency * 2
+    )
 
     app_base.wait_for_redis(sender, **kwargs)
     app_base.wait_for_db(sender, **kwargs)
