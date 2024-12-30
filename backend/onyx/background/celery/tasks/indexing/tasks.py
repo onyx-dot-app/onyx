ce32903914dae679a7d548cb4d5ec9fd7306e451
Richard Kuo <rkuo@rkuo.com>
2024-12-29 18:32:42 -0800

    comment some code so we don't change too many things at once

--- backend/onyx/background/celery/tasks/indexing/tasks.py
+++ backend/onyx/background/celery/tasks/indexing/tasks.py
@@ -105,17 +105,21 @@ class IndexingCallback(IndexingHeartbeatInterface):
         return False
 
     def progress(self, tag: str, amount: int) -> None:
-        if self.parent_pid:
-            # check if the parent pid is alive so we aren't running as a zombie
-            now = time.monotonic()
-            if now - self.last_parent_check > IndexingCallback.PARENT_CHECK_INTERVAL:
-                try:
-                    # this is unintuitive, but it checks if the parent pid is still running
-                    os.kill(self.parent_pid, 0)
-                except Exception:
-                    logger.exception("IndexingCallback - parent pid check exceptioned")
-                    raise
-                self.last_parent_check = now
+        # rkuo: this shouldn't be necessary yet because we spawn the process this runs inside
+        # with daemon = True. It seems likely some indexing tasks will need to spawn other processes eventually
+        # so leave this code in until we're ready to test it.
+
+        # if self.parent_pid:
+        #     # check if the parent pid is alive so we aren't running as a zombie
+        #     now = time.monotonic()
+        #     if now - self.last_parent_check > IndexingCallback.PARENT_CHECK_INTERVAL:
+        #         try:
+        #             # this is unintuitive, but it checks if the parent pid is still running
+        #             os.kill(self.parent_pid, 0)
+        #         except Exception:
+        #             logger.exception("IndexingCallback - parent pid check exceptioned")
+        #             raise
+        #         self.last_parent_check = now
 
         try:
             self.redis_lock.reacquire()
