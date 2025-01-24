38da48fe0389725e1beca4f36f0e480712f2291d
pablodanswer <pablo@danswer.ai>
2025-01-24 15:50:35 -0800

    update

--- web/src/components/CustomCheckbox.tsx
+++ web/src/components/CustomCheckbox.tsx
@@ -3,24 +3,31 @@ import { CheckmarkIcon } from "./icons/icons";
 export const CustomCheckbox = ({
   checked,
   onChange,
+  disabled,
 }: {
   checked: boolean;
   onChange?: () => void;
+  disabled?: boolean;
 }) => {
   return (
-    <label className="flex items-center cursor-pointer">
+    <label
+      className={`flex items-center cursor-pointer ${
+        disabled ? "opacity-50" : ""
+      }`}
+    >
       <input
         type="checkbox"
         className="hidden"
         checked={checked}
         onChange={onChange}
         readOnly={onChange ? false : true}
+        disabled={disabled}
       />
       <span className="relative">
         <span
           className={`block w-3 h-3 border border-border-strong rounded ${
             checked ? "bg-green-700" : "bg-background"
-          } transition duration-300`}
+          } transition duration-300 ${disabled ? "bg-background" : ""}`}
         >
           {checked && (
             <CheckmarkIcon
--- web/src/components/documentSet/DocumentSetSelectable.tsx
+++ web/src/components/documentSet/DocumentSetSelectable.tsx
@@ -1,15 +1,25 @@
 import { DocumentSet, ValidSources } from "@/lib/types";
 import { CustomCheckbox } from "../CustomCheckbox";
 import { SourceIcon } from "../SourceIcon";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export function DocumentSetSelectable({
   documentSet,
   isSelected,
   onSelect,
+  disabled,
+  disabledTooltip,
 }: {
   documentSet: DocumentSet;
   isSelected: boolean;
   onSelect: () => void;
+  disabled?: boolean;
+  disabledTooltip?: string;
 }) {
   const uniqueSources = new Set<ValidSources>();
   documentSet.cc_pair_descriptors.forEach((ccPairDescriptor) => {
@@ -17,38 +27,59 @@ export function DocumentSetSelectable({
   });
 
   return (
-    <div
-      key={documentSet.id}
-      className={
-        `
-        w-72
-        px-3 
-        py-1
-        rounded-lg 
-        border
-        border-border
-        flex 
-        cursor-pointer ` +
-        (isSelected ? " bg-hover" : " bg-background hover:bg-hover-light")
-      }
-      onClick={onSelect}
-    >
-      <div className="flex w-full">
-        <div className="flex flex-col h-full">
-          <div className="font-bold">{documentSet.name}</div>
-          <div className="text-xs">{documentSet.description}</div>
-          <div className="flex gap-x-2 pt-1 mt-auto mb-1">
-            {Array.from(uniqueSources).map((source) => (
-              <SourceIcon key={source} sourceType={source} iconSize={16} />
-            ))}
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger>
+          <div
+            key={documentSet.id}
+            className={
+              `
+              w-72
+              px-3 
+              py-1
+              rounded-lg 
+              border
+              border-border
+              ${disabled ? "bg-background" : ""}
+              flex 
+              cursor-pointer 
+              ` +
+              (isSelected ? " bg-hover" : " bg-background hover:bg-hover-light")
+            }
+            onClick={disabled ? () => null : onSelect}
+          >
+            <div className="flex w-full">
+              <div className="flex flex-col h-full">
+                <div className="font-bold">{documentSet.name}</div>
+                <div className="text-xs">{documentSet.description}</div>
+                <div className="flex gap-x-2 pt-1 mt-auto mb-1">
+                  {Array.from(uniqueSources).map((source) => (
+                    <SourceIcon
+                      key={source}
+                      sourceType={source}
+                      iconSize={16}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="ml-auto my-auto">
+                <div className="pl-1">
+                  <CustomCheckbox
+                    checked={isSelected}
+                    onChange={() => null}
+                    disabled={disabled}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="ml-auto my-auto">
-          <div className="pl-1">
-            <CustomCheckbox checked={isSelected} onChange={() => null} />
-          </div>
-        </div>
-      </div>
-    </div>
+        </TooltipTrigger>
+        {disabled && (
+          <TooltipContent>
+            <p>{disabledTooltip}</p>
+          </TooltipContent>
+        )}
+      </Tooltip>
+    </TooltipProvider>
   );
 }
