fa04e34375eb5ef6b5260f321327fd43c3be265c
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2025-02-07 14:42:50 +0200

    added timeout for playwright

--- backend/onyx/utils/playwright.py
+++ backend/onyx/utils/playwright.py
@@ -1,4 +1,7 @@
 import time
+from onyx.utils.logger import setup_logger
+
+logger = setup_logger()
 
 sites = ["https://water.europa.eu/freshwater",
         "https://water.europa.eu/marine",
@@ -8,13 +11,13 @@ sites = ["https://water.europa.eu/freshwater",
 threshold = 5
 
 def wait_for_page(url, page):
-    
+    logger.info(f"playwright attempt: {url}")
     found = False
     for site in sites:
         found = found or url.startswith(site)
     if not found:
         return page
-    page.wait_for_load_state("networkidle")
+    page.wait_for_load_state("networkidle", timeout=300000)
     page.wait_for_timeout(1000)
 
     heights = []
@@ -22,10 +25,9 @@ def wait_for_page(url, page):
         page.keyboard.press("PageDown");
 
         time.sleep(1)  # Give time for new content to load
-        page.wait_for_load_state("networkidle")
+        page.wait_for_load_state("networkidle", timeout=300000)
 
         heights.append(page.evaluate("document.body.scrollHeight"))
-        print(f"nh: {heights}")
         if len(heights) > threshold and heights[-1] == heights[-(1+threshold)]:
             break
     return page
