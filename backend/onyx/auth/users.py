a1f89c9123257413ab889aa43e99edde5afa9d81
pablodanswer <pablo@danswer.ai>
2025-02-14 15:19:44 -0800

    fix typing

--- backend/onyx/auth/users.py
+++ backend/onyx/auth/users.py
@@ -2,6 +2,7 @@ import json
 import secrets
 import uuid
 from collections.abc import AsyncGenerator
+from collections.abc import Awaitable
 from datetime import datetime
 from datetime import timezone
 from typing import cast
@@ -270,6 +271,7 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
             token = CURRENT_TENANT_ID_CONTEXTVAR.set(tenant_id)
             verify_email_is_invited(user_create.email)
             verify_email_domain(user_create.email)
+
             if MULTI_TENANT:
                 tenant_user_db = SQLAlchemyUserAdminDB[User, uuid.UUID](
                     db_session, User, OAuthAccount
@@ -289,8 +291,9 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
             try:
                 user = await super().create(user_create, safe=safe, request=request)  # type: ignore
             except exceptions.UserAlreadyExists:
-                # user = await self.get_by_email(user_create.email)
-                user = await tenant_user_db.get_by_email(user_create.email)
+                user = await cast(
+                    Awaitable[User], self.user_db.get_by_email(user_create.email)
+                )
 
                 # Handle case where user has used product outside of web and is now creating an account through web
                 if not user.role.is_web_login() and user_create.role.is_web_login():
