42e6f6a4a430da5b7296971a571c7ff4b8ad6294
marcustorm <44622206+marcustorm@users.noreply.github.com>
2025-03-26 23:59:59 +0100

    Update FixedLogo.tsx

--- web/src/components/logo/FixedLogo.tsx
+++ web/src/components/logo/FixedLogo.tsx
@@ -43,7 +43,6 @@ export const LogoComponent = memo(function LogoComponent({
             </HeaderTitle>
             {!NEXT_PUBLIC_DO_NOT_USE_TOGGLE_OFF_DANSWER_POWERED && (
               <p className="text-xs text-left text-subtle whitespace-nowrap overflow-hidden text-ellipsis">
-                Powered by Onyx
               </p>
             )}
           </div>
