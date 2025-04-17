9f969b7e85944008206ef7e9f4f09e804b60fa87
Vadim Severikov <camelot22rus@gmail.com>
2025-04-17 13:02:03 +0300

    фикс подстановки перевода

--- web/src/i18n/init.js
+++ web/src/i18n/init.js
@@ -5,10 +5,16 @@ const russian = require("./russian");
 i18next.init({
   lng: "ru",
   debug: true,
+  fallbackLng: "ru",
+  preload: ["ru"],
   resources: {
     ru: { translation: russian },
   },
-});
+}),
+  (err, t) => {
+    if (err) return console.log("something went wrong loading", err);
+    t("SIGN_UP_FOR_ONYX"); // -> same as i18next.t
+  };
 
 // Add this line to your app entrypoint. Usually it is src/index.js
 // import './i18n/init';
