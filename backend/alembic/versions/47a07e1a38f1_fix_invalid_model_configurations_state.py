08b3f7036f213e537ddab19f3eb94553025e33cf
Raunak Bhagat <r@rabh.io>
2025-04-23 20:19:06 -0700

    Finish script to revalidate `display_model_names`

--- backend/alembic/versions/47a07e1a38f1_fix_invalid_model_configurations_state.py
+++ backend/alembic/versions/47a07e1a38f1_fix_invalid_model_configurations_state.py
@@ -10,7 +10,17 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
-from onyx.llm.llm_provider_options import fetch_model_names_for_provider_as_set
+from onyx.llm.llm_provider_options import (
+    ANTHROPIC_PROVIDER_NAME,
+    ANTHROPIC_VISIBLE_MODEL_NAMES,
+    BEDROCK_PROVIDER_NAME,
+    OPEN_AI_VISIBLE_MODEL_NAMES,
+    OPENAI_PROVIDER_NAME,
+    VERTEXAI_DEFAULT_FAST_MODEL,
+    VERTEXAI_DEFAULT_MODEL,
+    VERTEXAI_PROVIDER_NAME,
+    fetch_model_names_for_provider_as_set,
+)
 
 
 # revision identifiers, used by Alembic.
@@ -18,6 +28,14 @@ revision = "47a07e1a38f1"
 down_revision = "7a70b7664e37"
 branch_labels = None
 depends_on = None
+
+
+_PROVIDER_TO_VISIBLE_MODELS_MAP = {
+    OPENAI_PROVIDER_NAME: set(OPEN_AI_VISIBLE_MODEL_NAMES),
+    BEDROCK_PROVIDER_NAME: set(),  # TODO!
+    ANTHROPIC_PROVIDER_NAME: set(ANTHROPIC_VISIBLE_MODEL_NAMES),
+    VERTEXAI_PROVIDER_NAME: set([VERTEXAI_DEFAULT_MODEL, VERTEXAI_DEFAULT_FAST_MODEL]),
+}
 
 
 def upgrade() -> None:
@@ -58,24 +76,61 @@ def upgrade() -> None:
         if not models:
             continue
 
-        _model_configurations = connection.execute(
-            sa.select(
-                model_configuration_table.c.id,
-                model_configuration_table.c.llm_provider_id,
-                model_configuration_table.c.name,
-                model_configuration_table.c.is_visible,
-            ).where(model_configuration_table.c.llm_provider_id == llm_provider_id)
-        ).fetchall()
+        model_configurations = list(
+            connection.execute(
+                sa.select(
+                    model_configuration_table.c.id,
+                    model_configuration_table.c.llm_provider_id,
+                    model_configuration_table.c.name,
+                    model_configuration_table.c.is_visible,
+                ).where(model_configuration_table.c.llm_provider_id == llm_provider_id)
+            ).fetchall()
+        )
 
-        all_are_private = True
+        display_model_names: set[str] = _PROVIDER_TO_VISIBLE_MODELS_MAP[provider_name]
 
-        # If `not all_are_private`, then there must exist at least one
-        # model which is public. Therefore, this is a valid state;
-        # don't touch this state and move onto the next one.
-        if not all_are_private:
-            continue
+        if model_configurations:
+            at_least_one_is_public = any(
+                [model_configuration[3] for model_configuration in model_configurations]
+            )
 
-        ...
+            # If there is at least one model which is public, this is a valid state.
+            # Therefore, don't touch it and move on to the next one.
+            if at_least_one_is_public:
+                continue
+
+            existing_visible_model_names: set[str] = set(
+                [
+                    model_configuration[2]
+                    for model_configuration in model_configurations
+                    if model_configuration[3]
+                ]
+            )
+
+            for model_name in display_model_names.difference(
+                existing_visible_model_names
+            ):
+                if not model_name:
+                    continue
+
+                connection.execute(
+                    model_configuration_table.insert().values(
+                        llm_provider_id=llm_provider_id,
+                        name=model_name,
+                        is_visible=True,
+                        max_input_tokens=None,
+                    )
+                )
+        else:
+            for model_name in models:
+                connection.execute(
+                    model_configuration_table.insert().values(
+                        llm_provider_id=llm_provider_id,
+                        name=model_name,
+                        is_visible=model_name in display_model_names,
+                        max_input_tokens=None,
+                    )
+                )
 
 
 def downgrade() -> None:
