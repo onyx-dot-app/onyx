3ddf5a749dfe94dfdc9110251355f676ffef40ec
Raunak Bhagat <r@rabh.io>
2025-04-28 09:44:22 -0700

    Update download component

--- web/src/app/ee/admin/performance/query-history/DownloadAsCSV.tsx
+++ web/src/app/ee/admin/performance/query-history/DownloadAsCSV.tsx
@@ -1,13 +1,11 @@
+import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
 export function DownloadAsCSV() {
   return (
-    <a
-      href="/api/admin/query-history-csv"
-      className="flex ml-auto py-2 px-4 border border-border h-fit cursor-pointer hover:bg-accent-background text-sm"
-    >
+    <Button className="flex ml-auto py-2 px-4 border border-border h-fit cursor-pointer hover:bg-accent-background text-sm">
       <FiDownload className="my-auto mr-2" />
       Download as CSV
-    </a>
+    </Button>
   );
 }
