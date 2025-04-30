1b9c59bca266abf1c4c92ebf05c4a4be5c7f3038
Raunak Bhagat <r@rabh.io>
2025-04-29 19:47:07 -0700

    Update return code

--- backend/ee/onyx/server/query_history/api.py
+++ backend/ee/onyx/server/query_history/api.py
@@ -321,7 +321,7 @@ def download_query_history_csv(
     # Maybe we should handle each specific TaskStatus separately?
     # I.e., if it's a `TaskStatus.PENDING` return a different error code vs if it's a `TaskStatus.FAILURE`.
     if task.status in [TaskStatus.STARTED, TaskStatus.PENDING]:
-        raise HTTPException(102, f"Task with {request_id=} is still being worked on")
+        raise HTTPException(202, f"Task with {request_id=} is still being worked on")
 
     elif task.status == TaskStatus.FAILURE:
         raise HTTPException(500, f"Task with {request_id=} failed")
