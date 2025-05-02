27ae953638e55ec47f7d1bc2a49ee846b57d9420
Raunak Bhagat <r@rabh.io>
2025-05-01 19:21:42 -0700

    Decrease interval time for re-pinging API

--- web/src/app/ee/admin/performance/query-history/constants.ts
+++ web/src/app/ee/admin/performance/query-history/constants.ts
@@ -5,7 +5,7 @@ export const CHECK_QUERY_HISTORY_EXPORT_STATUS_URL =
   "/api/admin/query-history/export-status";
 export const DOWNLOAD_QUERY_HISTORY_URL = "/api/admin/query-history/download";
 export const MAX_RETRIES = 10;
-export const RETRY_COOLDOWN_MILLISECONDS = 1000;
+export const RETRY_COOLDOWN_MILLISECONDS = 200;
 
 export const ITEMS_PER_PAGE = 20;
 export const PAGES_PER_BATCH = 2;
