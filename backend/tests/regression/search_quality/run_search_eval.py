877b455942caa7ac369f8411efeefcfb258ef7ba
Rei Meguro <36625832+Orbital-Web@users.noreply.github.com>
2025-05-14 15:54:21 -0700

    fix: import

--- backend/tests/regression/search_quality/run_search_eval.py
+++ backend/tests/regression/search_quality/run_search_eval.py
@@ -1,6 +1,8 @@
+import csv
 import json
 import os
 from dataclasses import dataclass
+from datetime import datetime
 
 import yaml
 from sqlalchemy.orm import Session
@@ -18,19 +20,19 @@ from onyx.context.search.models import InferenceChunkUncleaned
 from onyx.context.search.preprocessing.preprocessing import query_analysis
 from onyx.context.search.retrieval.search_runner import get_query_embedding
 from onyx.context.search.utils import remove_stop_words_and_punctuation
-from onyx.db.engine import get_session_context_manager
+from onyx.db.engine import get_session_with_current_tenant
 from onyx.db.engine import SqlEngine
 from onyx.db.search_settings import get_current_search_settings
 from onyx.db.search_settings import get_multilingual_expansion
 from onyx.document_index.factory import get_default_document_index
-from onyx.document_index.vespa.index import VespaIndex
+from onyx.document_index.interfaces import DocumentIndex
 from onyx.utils.logger import setup_logger
 
 logger = setup_logger(__name__)
 
 
 @dataclass
-class SearchParameters:
+class SearchEvalParameters:
     hybrid_alpha: float
     hybrid_alpha_keyword: float
     doc_time_decay: float
@@ -39,15 +41,19 @@ class SearchParameters:
     offset: int
     title_content_ratio: float
     user_email: str | None
+    export_file: str
 
 
-def _load_search_parameters() -> SearchParameters:
+def _load_search_parameters() -> SearchEvalParameters:
     current_dir = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(current_dir, "search_eval_config.yaml")
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
 
-    return SearchParameters(
+    export_file = config.get("EXPORT_FILE", "search-%Y-%m-%d-%H-%M-%S")
+    export_file = datetime.now().strftime(export_file)
+
+    search_parameters = SearchEvalParameters(
         hybrid_alpha=config.get("HYBRID_ALPHA") or HYBRID_ALPHA,
         hybrid_alpha_keyword=config.get("HYBRID_ALPHA_KEYWORD") or HYBRID_ALPHA_KEYWORD,
         doc_time_decay=config.get("DOC_TIME_DECAY") or DOC_TIME_DECAY,
@@ -56,7 +62,17 @@ def _load_search_parameters() -> SearchParameters:
         offset=config.get("OFFSET") or 0,
         title_content_ratio=config.get("TITLE_CONTENT_RATIO") or TITLE_CONTENT_RATIO,
         user_email=config.get("USER_EMAIL"),
+        export_file=export_file + ".csv",
     )
+    logger.info(f"Using search parameters: {search_parameters}")
+
+    logger.info(f"Exporting config to {export_file + '.json'}")
+    with open(export_file + ".json", "w") as file:
+        search_parameters_dict = search_parameters.__dict__
+        search_parameters_dict["rank_profile"] = search_parameters.rank_profile.value
+        json.dump(search_parameters_dict, file, indent=4)
+
+    return search_parameters
 
 
 def _load_queries() -> list[str]:
@@ -69,9 +85,9 @@ def _load_queries() -> list[str]:
 def _search_one_query(
     query: str,
     multilingual_expansion: list[str],
-    document_index: VespaIndex,
+    document_index: DocumentIndex,
     db_session: Session,
-    search_parameters: SearchParameters,
+    search_parameters: SearchEvalParameters,
 ) -> list[InferenceChunkUncleaned]:
     # note that normally query refers to the modified query
     # here, we're sending the original query so the query doesn't randomly change
@@ -123,25 +139,32 @@ def run_search_eval() -> None:
     )
 
     search_parameters = _load_search_parameters()
-    logger.info(f"Using search parameters: {search_parameters}")
-
     queries = _load_queries()
 
-    with get_session_context_manager() as db_session:
+    with get_session_with_current_tenant() as db_session:
         search_settings = get_current_search_settings(db_session)
         document_index = get_default_document_index(search_settings, None)
         multilingual_expansion = get_multilingual_expansion(db_session)
 
-        for query in queries:
-            results = _search_one_query(
-                query,
-                multilingual_expansion,
-                document_index,
-                db_session,
-                search_parameters,
-            )
-            # TODO: evaluate results rather than printing it
-            print(results)
+        with open(search_parameters.export_file, "w") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(["query", "rank", "score", "doc_id", "chunk_id"])
+
+            for query in queries:
+                results = _search_one_query(
+                    query,
+                    multilingual_expansion,
+                    document_index,
+                    db_session,
+                    search_parameters,
+                )
+
+                for rank, result in enumerate(results):
+                    csv_writer.writerow(
+                        [query, rank, result.score, result.document_id, result.chunk_id]
+                    )
+
+    logger.info(f"Exported results to {search_parameters.export_file}")
 
 
 if __name__ == "__main__":
