2a795a150be83fc83e0216d273ad1f6c891883bc
Steven Takeshita <steven.hiroshi@gmail.com>
2025-05-22 19:09:36 -0400

    no more

--- web/src/components/logo/FixedLogo.tsx
+++ web/src/components/logo/FixedLogo.tsx
@@ -41,11 +41,6 @@ export const LogoComponent = memo(function LogoComponent({
             <HeaderTitle backgroundToggled={backgroundToggled}>
               {enterpriseSettings.application_name}
             </HeaderTitle>
-            {!NEXT_PUBLIC_DO_NOT_USE_TOGGLE_OFF_DANSWER_POWERED && (
-              <p className="text-xs text-left text-subtle whitespace-nowrap overflow-hidden text-ellipsis">
-                Powered by Onyx
-              </p>
-            )}
           </div>
         </>
       ) : (
