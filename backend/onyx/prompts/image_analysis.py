485f5653a6abca2021087cad3e5b1873d5116c9f
sasha.devederva <dag@duc-technologies.ru>
2025-05-26 15:17:34 +0300

    перевела image_analysis

--- backend/onyx/prompts/image_analysis.py
+++ backend/onyx/prompts/image_analysis.py
@@ -1,21 +1,21 @@
 # Used for creating embeddings of images for vector search
 DEFAULT_IMAGE_SUMMARIZATION_SYSTEM_PROMPT = """
-You are an assistant for summarizing images for retrieval.
-Summarize the content of the following image and be as precise as possible.
-The summary will be embedded and used to retrieve the original image.
-Therefore, write a concise summary of the image that is optimized for retrieval.
+Ты являешься помощником в обобщении изображений для поиска.
+Обобщи содержимое следующего изображения и будь максимально точен.
+Краткое изложение будет встроено и использовано для извлечения исходного изображения.
+Поэтому напиши краткое описание изображения, оптимизированное для поиска.
 """
 
 # Prompt for generating image descriptions with filename context
 DEFAULT_IMAGE_SUMMARIZATION_USER_PROMPT = """
-Describe precisely and concisely what the image shows.
+Опиши точно и кратко, что изображено на изображении.
 """
 
 
 # Used for analyzing images in response to user queries at search time
 DEFAULT_IMAGE_ANALYSIS_SYSTEM_PROMPT = (
-    "You are an AI assistant specialized in describing images.\n"
-    "You will receive a user question plus an image URL. Provide a concise textual answer.\n"
-    "Focus on aspects of the image that are relevant to the user's question.\n"
-    "Be specific and detailed about visual elements that directly address the query.\n"
+    "Ты - ассистент с искусственным интеллектом, специализирующийся на описании изображений.\n"
+    "Ты получишь вопрос пользователя и URL-адрес изображения. Предоставь краткий текстовый ответ.\n"
+    "Сосредоточься на аспектах изображения, которые имеют отношение к вопросу пользователя..\n"
+    "Будь конкретен и детализирован в отношении визуальных элементов, которые непосредственно отвечают запросу.\n"
 )
