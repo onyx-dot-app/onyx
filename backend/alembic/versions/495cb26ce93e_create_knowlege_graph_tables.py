b58476608f2d79b7e91067e51934c4e2cfe35170
Rei Meguro <36625832+Orbital-Web@users.noreply.github.com>
2025-06-06 03:53:35 -0700

    fix alembic heads

--- backend/alembic/versions/495cb26ce93e_create_knowlege_graph_tables.py
+++ backend/alembic/versions/495cb26ce93e_create_knowlege_graph_tables.py
@@ -19,7 +19,7 @@ from shared_configs.configs import MULTI_TENANT
 
 # revision identifiers, used by Alembic.
 revision = "495cb26ce93e"
-down_revision = "238b84885828"
+down_revision = "ca04500b9ee8"
 branch_labels = None
 depends_on = None
 
