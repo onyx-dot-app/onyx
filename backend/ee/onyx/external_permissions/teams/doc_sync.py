4fd7b0aca606e069024c5a08ba7dc36841a046e7
Raunak Bhagat <r@rabh.io>
2025-06-24 14:44:23 -0700

    Simplify how doc-sync is done

--- backend/ee/onyx/external_permissions/teams/doc_sync.py
+++ backend/ee/onyx/external_permissions/teams/doc_sync.py
@@ -1,8 +1,9 @@
 from collections.abc import Generator
 
 from ee.onyx.external_permissions.perm_sync_types import FetchAllDocumentsFunction
-from ee.onyx.external_permissions.utils import make_missing_docs_inaccessible
+from ee.onyx.external_permissions.utils import generic_doc_sync
 from onyx.access.models import DocExternalAccess
+from onyx.configs.constants import DocumentSource
 from onyx.connectors.teams.connector import TeamsConnector
 from onyx.db.models import ConnectorCredentialPair
 from onyx.indexing.indexing_heartbeat import IndexingHeartbeatInterface
@@ -19,38 +20,16 @@ def teams_doc_sync(
     fetch_all_existing_docs_fn: FetchAllDocumentsFunction,
     callback: IndexingHeartbeatInterface | None,
 ) -> Generator[DocExternalAccess, None, None]:
-    logger.info(f"Starting teams doc sync for CC Pair ID: {cc_pair.id}")
     teams_connector = TeamsConnector(
         **cc_pair.connector.connector_specific_config,
     )
     teams_connector.load_credentials(cc_pair.credential.credential_json)
 
-    logger.info(f"Querying existing document IDs for CC Pair ID: {cc_pair.id}")
-    existing_doc_ids = fetch_all_existing_docs_fn()
-
-    logger.info("Fetching all slim documents from teams")
-    for doc_batch in teams_connector.retrieve_all_slim_documents(callback=callback):
-        logger.info(f"Got {len(doc_batch)} slim documents from teams")
-
-        yield from make_missing_docs_inaccessible(
-            fetched_slim_docs=doc_batch,
-            existing_doc_ids=existing_doc_ids,
-        )
-
-        for doc in doc_batch:
-            if not doc.external_access:
-                raise RuntimeError(
-                    f"No external access found for document ID: {doc.id}"
-                )
-
-            yield DocExternalAccess(
-                doc_id=doc.id,
-                external_access=doc.external_access,
-            )
-
-        if callback:
-            if callback.should_stop():
-                raise RuntimeError(f"{TEAMS_DOC_SYNC_LABEL}: Stop signal detected")
-            callback.progress(TEAMS_DOC_SYNC_LABEL, 1)
-
-    logger.info("Finished teams doc sync")
+    yield from generic_doc_sync(
+        cc_pair=cc_pair,
+        fetch_all_existing_docs_fn=fetch_all_existing_docs_fn,
+        callback=callback,
+        doc_source=DocumentSource.TEAMS,
+        slim_connector=teams_connector,
+        label=TEAMS_DOC_SYNC_LABEL,
+    )
--- backend/ee/onyx/external_permissions/utils.py
+++ backend/ee/onyx/external_permissions/utils.py
@@ -1,8 +1,13 @@
 from collections.abc import Generator
 
+from ee.onyx.external_permissions.perm_sync_types import FetchAllDocumentsFunction
 from onyx.access.models import DocExternalAccess
 from onyx.access.models import ExternalAccess
+from onyx.configs.constants import DocumentSource
+from onyx.connectors.interfaces import SlimConnector
 from onyx.connectors.models import SlimDocument
+from onyx.db.models import ConnectorCredentialPair
+from onyx.indexing.indexing_heartbeat import IndexingHeartbeatInterface
 from onyx.utils.logger import setup_logger
 
 logger = setup_logger()
@@ -18,7 +23,6 @@ def make_missing_docs_inaccessible(
 
     Each one of the fetched `SlimDocument`'s `DocExternalAccess` will be yielded.
     """
-
     fetched_ids = {doc.id for doc in fetched_slim_docs}
     existing_ids = set(existing_doc_ids)
 
@@ -34,3 +38,47 @@ def make_missing_docs_inaccessible(
                 doc_id=missing_id,
                 external_access=ExternalAccess.empty(),
             )
+
+
+def generic_doc_sync(
+    cc_pair: ConnectorCredentialPair,
+    fetch_all_existing_docs_fn: FetchAllDocumentsFunction,
+    callback: IndexingHeartbeatInterface | None,
+    doc_source: DocumentSource,
+    slim_connector: SlimConnector,
+    label: str,
+) -> Generator[DocExternalAccess, None, None]:
+    """
+    TODO
+    """
+    logger.info(f"Starting {doc_source} doc sync for CC Pair ID: {cc_pair.id}")
+
+    logger.info(f"Querying existing document IDs for CC Pair ID: {cc_pair.id}")
+    existing_doc_ids = fetch_all_existing_docs_fn()
+
+    logger.info(f"Fetching all slim documents from {doc_source}")
+    for doc_batch in slim_connector.retrieve_all_slim_documents(callback=callback):
+        logger.info(f"Got {len(doc_batch)} slim documents from {doc_source}")
+
+        yield from make_missing_docs_inaccessible(
+            fetched_slim_docs=doc_batch,
+            existing_doc_ids=existing_doc_ids,
+        )
+
+        for doc in doc_batch:
+            if not doc.external_access:
+                raise RuntimeError(
+                    f"No external access found for document ID: {doc.id}"
+                )
+
+            yield DocExternalAccess(
+                doc_id=doc.id,
+                external_access=doc.external_access,
+            )
+
+        if callback:
+            if callback.should_stop():
+                raise RuntimeError(f"{label}: Stop signal detected")
+            callback.progress(label, 1)
+
+    logger.info(f"Finished {doc_source} doc sync")
