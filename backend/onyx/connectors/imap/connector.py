b4ebe544b033e80efc9043cb6b15e69c222897e0
Raunak Bhagat <r@rabh.io>
2025-07-07 09:02:50 -0700

    Implement checkpointed imap-connector

--- backend/onyx/connectors/imap/connector.py
+++ backend/onyx/connectors/imap/connector.py
@@ -1,8 +1,10 @@
+import copy
 import email
 import imaplib
 from email.message import Message
 from email.utils import parseaddr
 from typing import Any
+from typing import cast
 
 import bs4
 
@@ -20,16 +22,17 @@ from onyx.connectors.models import ConnectorCheckpoint
 from onyx.connectors.models import Document
 from onyx.connectors.models import TextSection
 from onyx.utils.logger import setup_logger
-from tests.daily.connectors.utils import load_all_docs_from_checkpoint_connector
 
 logger = setup_logger()
 
 
-DEFAULT_IMAP_PORT_NUMBER = 993
-IMAP_OKAY_STATUS = "OK"
+_DEFAULT_IMAP_PORT_NUMBER = 993
+_IMAP_OKAY_STATUS = "OK"
+_PAGE_SIZE = 100
 
 
-class ImapCheckpoint(ConnectorCheckpoint): ...
+class ImapCheckpoint(ConnectorCheckpoint):
+    todo_email_ids: list[str] = []
 
 
 class ImapConnector(
@@ -39,7 +42,7 @@ class ImapConnector(
     def __init__(
         self,
         host: str,
-        port: int = DEFAULT_IMAP_PORT_NUMBER,
+        port: int = _DEFAULT_IMAP_PORT_NUMBER,
     ) -> None:
         self._host = host
         self._port = port
@@ -84,40 +87,66 @@ class ImapConnector(
         end: SecondsSinceUnixEpoch,
         checkpoint: ImapCheckpoint,
     ) -> CheckpointOutput[ImapCheckpoint]:
-        status, _ids = self._mail_client.select("Inbox", readonly=True)
-        if status != IMAP_OKAY_STATUS:
-            raise RuntimeError
+        if not checkpoint.todo_email_ids:
+            # This is the first time running this connector.
+            # Populate the todo_email_ids and return a checkpoint.
+            email_ids = _fetch_email_ids(mail_client=self._mail_client)
+            return ImapCheckpoint(has_more=True, todo_email_ids=email_ids)
 
-        status, email_ids = self._mail_client.search(None, "ALL")
+        current_todos = cast(
+            list, copy.deepcopy(checkpoint.todo_email_ids[:_PAGE_SIZE])
+        )
+        future_todos = cast(list, copy.deepcopy(checkpoint.todo_email_ids[_PAGE_SIZE:]))
 
-        if status != IMAP_OKAY_STATUS or not email_ids:
-            raise RuntimeError
-
-        for email_id in email_ids[0].split():
-            status, msg_data = self._mail_client.fetch(
-                message_set=email_id, message_parts="(RFC822)"
-            )
-            if status != IMAP_OKAY_STATUS or not msg_data:
+        for email_id in current_todos:
+            email_msg = _fetch_email(mail_client=self._mail_client, email_id=email_id)
+            if not email_msg:
+                logger.warn(f"Failed to fetch message {email_id=}; skipping")
                 continue
-            data = msg_data[0]
-            if not isinstance(data, tuple):
-                continue
-            _data, raw_email = data
 
-            email_msg = email.message_from_bytes(raw_email)
             email_headers = EmailHeaders.from_email_msg(email_msg=email_msg)
 
             yield _convert_email_headers_and_body_into_document(
                 email_msg=email_msg, email_headers=email_headers
             )
 
-        return ImapCheckpoint(has_more=False)
+        return ImapCheckpoint(has_more=bool(future_todos), todo_email_ids=future_todos)
 
     def build_dummy_checkpoint(self) -> ImapCheckpoint:
         return ImapCheckpoint(has_more=True)
 
     def validate_checkpoint_json(self, checkpoint_json: str) -> ImapCheckpoint:
         raise NotImplementedError
+
+
+def _fetch_email(mail_client: imaplib.IMAP4_SSL, email_id: str) -> Message | None:
+    status, msg_data = mail_client.fetch(message_set=email_id, message_parts="(RFC822)")
+    if status != _IMAP_OKAY_STATUS or not msg_data:
+        return None
+
+    data = msg_data[0]
+    if not isinstance(data, tuple):
+        raise RuntimeError(
+            f"Message data should be a tuple; instead got a {type(data)=} {data=}"
+        )
+
+    _other, raw_email = data
+    return email.message_from_bytes(raw_email)
+
+
+def _fetch_email_ids(mail_client: imaplib.IMAP4_SSL) -> list[str]:
+    status, _ids = mail_client.select("Inbox", readonly=True)
+    if status != _IMAP_OKAY_STATUS:
+        raise RuntimeError
+
+    status, email_ids_byte_array = mail_client.search(None, "ALL")
+
+    if status != _IMAP_OKAY_STATUS or not email_ids_byte_array:
+        raise RuntimeError(f"Failed to fetch email ids; {status=}")
+
+    email_ids: bytes = email_ids_byte_array[0]
+
+    return [email_id.decode() for email_id in email_ids.split()]
 
 
 def _convert_email_headers_and_body_into_document(
@@ -210,10 +239,17 @@ if __name__ == "__main__":
         )
     )
 
-    for doc in load_all_docs_from_checkpoint_connector(
-        connector=imap_connector,
-        start=0,
-        end=time.time(),
-    ):
-        print(doc)
-        ...
+    checkpoint = imap_connector.build_dummy_checkpoint()
+    while True:
+        for doc in imap_connector.load_from_checkpoint(
+            start=0.0, end=time.time(), checkpoint=checkpoint
+        ):
+            print(doc)
+
+    # for doc in load_all_docs_from_checkpoint_connector(
+    #     connector=imap_connector,
+    #     start=0,
+    #     end=time.time(),
+    # ):
+    #     print(doc)
+    #     ...
