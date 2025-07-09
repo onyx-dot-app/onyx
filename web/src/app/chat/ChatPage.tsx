5542ad8e81b70784b38d1d3a3a21d1a83230084a
Raunak Bhagat <r@rabh.io>
2025-07-09 12:08:48 -0700

    Have document show up before message starts streaming back

--- web/src/app/chat/ChatPage.tsx
+++ web/src/app/chat/ChatPage.tsx
@@ -203,6 +203,8 @@ export function ChatPage({
     refetch: refetchFederatedConnectors,
   } = useFederatedOAuthStatus();
 
+  const [messageFiles, setMessageFiles] = useState<FileDescriptor[]>([]);
+
   // Also fetch federated connectors for the sources list
   const { data: federatedConnectorsData } = useFederatedConnectors();
 
@@ -1238,6 +1240,13 @@ export function ChatPage({
     // If under the context limit, the files will be included in the chat history
     // so we don't need to keep them around.
     if (selectedDocumentTokens < maxTokens) {
+      setMessageFiles(
+        selectedFiles.map((selectedFile) => ({
+          id: selectedFile.id.toString(),
+          type: selectedFile.chat_file_type,
+          name: selectedFile.name,
+        }))
+      );
       setSelectedFiles([]);
     }
 
@@ -3357,6 +3366,7 @@ export function ChatPage({
                                 key={-2}
                                 messageId={-1}
                                 content={submittedMessage}
+                                files={messageFiles}
                               />
                             )}
 
