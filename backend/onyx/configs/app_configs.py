cfad7b0caeb62169c9f202d17ee61cee8a8f65ee
Weves <chrisweaver101@gmail.com>
2025-08-04 19:31:28 -0700

    Address comments

--- backend/onyx/configs/app_configs.py
+++ backend/onyx/configs/app_configs.py
@@ -360,8 +360,9 @@ POLL_CONNECTOR_OFFSET = 30  # Minutes overlap between poll windows
 ENABLED_CONNECTOR_TYPES = os.environ.get("ENABLED_CONNECTOR_TYPES") or ""
 
 # If set to true, curators can only access and edit assistants that they created
-CURATORS_CANNOT_EDIT_NON_OWNED_ASSISTANTS = (
-    os.environ.get("CURATORS_CANNOT_EDIT_NON_OWNED_ASSISTANTS", "").lower() == "true"
+CURATORS_CANNOT_VIEW_OR_EDIT_NON_OWNED_ASSISTANTS = (
+    os.environ.get("CURATORS_CANNOT_VIEW_OR_EDIT_NON_OWNED_ASSISTANTS", "").lower()
+    == "true"
 )
 
 # Some calls to get information on expert users are quite costly especially with rate limiting
