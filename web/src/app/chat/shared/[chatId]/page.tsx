dcb6de8df8bd91162b5dd9df32df62f90f665fea
Cameron <github@camro.net>
2025-08-18 10:10:17 +1000

    Revert changes to chat

--- web/src/app/chat/shared/[chatId]/page.tsx
+++ web/src/app/chat/shared/[chatId]/page.tsx
@@ -47,7 +47,7 @@ export default async function Page(props: {
 
   const authDisabled = authTypeMetadata?.authType === "disabled";
   if (!authDisabled && !user) {
-    return redirect(`/auth/login?next=/chat/shared/${params.chatId}`);
+    return redirect("/auth/login");
   }
 
   if (user && !user.is_verified && authTypeMetadata?.requiresVerification) {
