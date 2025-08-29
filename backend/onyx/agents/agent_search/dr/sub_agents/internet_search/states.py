d7085dc916d87999053b45d7e901bcc9dafbc690
Richard Guan <rguan72@gmail.com>
2025-08-28 18:26:21 -0700

    .

--- backend/onyx/agents/agent_search/dr/sub_agents/internet_search/states.py
+++ backend/onyx/agents/agent_search/dr/sub_agents/internet_search/states.py
@@ -6,18 +6,13 @@ from onyx.agents.agent_search.dr.sub_agents.states import BranchInput
 from onyx.agents.agent_search.dr.sub_agents.states import SubAgentInput
 
 
-def dict_op():
-    def _f(d1: dict, d2: dict):
-        return {**d1, **d2}
-
-    return _f
-
-
 class InternetSearchInput(SubAgentInput):
     urls_to_open: Annotated[list[tuple[str, str]], add] = []
     parallelization_nr: Annotated[int, add] = 0
     branch_question: Annotated[str, add] = ""
-    deduped_branch_question_to_urls: Annotated[dict[str, list[str]], dict_op()] = {}
+    deduped_branch_question_to_urls: Annotated[
+        dict[str, list[str]], lambda d1, d2: {**d1, **d2}
+    ] = {}
 
 
 class InternetSearchUpdate(LoggerUpdate):
