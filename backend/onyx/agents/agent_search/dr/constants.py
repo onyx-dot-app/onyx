1f0f1c785defbd8da95518945640fd395707a884
joachim-danswer <joachim@danswer.ai>
2025-09-02 08:21:31 -0700

    web search cost increase

--- backend/onyx/agents/agent_search/dr/constants.py
+++ backend/onyx/agents/agent_search/dr/constants.py
@@ -18,7 +18,7 @@ HIGH_LEVEL_PLAN_PREFIX = "The Plan:"
 AVERAGE_TOOL_COSTS: dict[DRPath, float] = {
     DRPath.INTERNAL_SEARCH: 1.0,
     DRPath.KNOWLEDGE_GRAPH: 2.0,
-    DRPath.WEB_SEARCH: 1.0,
+    DRPath.WEB_SEARCH: 1.5,
     DRPath.IMAGE_GENERATION: 3.0,
     DRPath.GENERIC_TOOL: 1.5,  # TODO: see todo in OrchestratorTool
     DRPath.CLOSER: 0.0,
