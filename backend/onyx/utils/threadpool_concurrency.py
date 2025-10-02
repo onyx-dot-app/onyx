d5ea8ff94f23ae6aa0a6eadd05436848ba88c193
Richard Guan <rguan72@gmail.com>
2025-10-02 16:40:06 -0700

    .

--- backend/onyx/utils/threadpool_concurrency.py
+++ backend/onyx/utils/threadpool_concurrency.py
@@ -289,8 +289,8 @@ def run_async_sync(coro: Awaitable[T]) -> T:
     context = contextvars.copy_context()
     with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
         future: concurrent.futures.Future[T] = executor.submit(
-+            context.run, asyncio.run, coro
-+       )
+            context.run, asyncio.run, coro
+        )
         return future.result()
 
 class TimeoutThread(threading.Thread, Generic[R]):
