7d1c0434594be59c7b721611cec1ecf034fe321c
Raunak Bhagat <r@rabh.io>
2025-10-09 17:20:24 -0700

    Fix share modal

--- web/src/app/chat/components/modal/ShareChatSessionModal.tsx
+++ web/src/app/chat/components/modal/ShareChatSessionModal.tsx
@@ -111,150 +111,147 @@ export default function ShareChatSessionModal({
   const updateCurrentChatSessionSharedStatus = useChatSessionStore(
     (state) => state.updateCurrentChatSessionSharedStatus
   );
-  const { ModalProvider } = createModalProvider();
 
   return (
     <>
       {popup}
 
-      <ModalProvider>
-        <ConfirmationModalContent
-          icon={SvgShare}
-          title="Share Chat"
-          submit={<Button>Share</Button>}
-        >
-          {shareLink ? (
-            <div>
-              <Text>
-                This chat session is currently shared. Anyone in your team can
-                view the message history using the following link:
-              </Text>
+      <ConfirmationModalContent
+        icon={SvgShare}
+        title="Share Chat"
+        submit={<Button>Share</Button>}
+      >
+        {shareLink ? (
+          <div>
+            <Text>
+              This chat session is currently shared. Anyone in your team can
+              view the message history using the following link:
+            </Text>
 
-              <div className={cn("flex mt-2")}>
-                {/* <CopyButton content={shareLink} /> */}
-                <IconButton
-                  icon={SvgCopy}
-                  onClick={() => copyAll(shareLink)}
-                  secondary
-                />
-                <a
-                  href={shareLink}
-                  target="_blank"
-                  className={cn(
-                    "underline mt-1 ml-1 text-sm my-auto",
-                    "text-action-link-05"
-                  )}
-                  rel="noreferrer"
-                >
-                  {shareLink}
-                </a>
-              </div>
+            <div className={cn("flex mt-2")}>
+              {/* <CopyButton content={shareLink} /> */}
+              <IconButton
+                icon={SvgCopy}
+                onClick={() => copyAll(shareLink)}
+                secondary
+              />
+              <a
+                href={shareLink}
+                target="_blank"
+                className={cn(
+                  "underline mt-1 ml-1 text-sm my-auto",
+                  "text-action-link-05"
+                )}
+                rel="noreferrer"
+              >
+                {shareLink}
+              </a>
+            </div>
 
-              <Separator />
+            <Separator />
 
-              <Text className={cn("mb-4")}>
-                Click the button below to make the chat private again.
-              </Text>
+            <Text className={cn("mb-4")}>
+              Click the button below to make the chat private again.
+            </Text>
 
-              <Button
-                onClick={async () => {
-                  const success = await deleteShareLink(chatSession.id);
-                  if (success) {
-                    setShareLink("");
-                    updateCurrentChatSessionSharedStatus(
-                      ChatSessionSharedStatus.Private
-                    );
+            <Button
+              onClick={async () => {
+                const success = await deleteShareLink(chatSession.id);
+                if (success) {
+                  setShareLink("");
+                  updateCurrentChatSessionSharedStatus(
+                    ChatSessionSharedStatus.Private
+                  );
+                } else {
+                  alert("Failed to delete share link");
+                }
+              }}
+              danger
+            >
+              Delete Share Link
+            </Button>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-spacing-interline">
+            <Callout type="warning" title="Warning">
+              Please make sure that all content in this chat is safe to share
+              with the whole team.
+            </Callout>
+            <Button
+              leftIcon={SvgCopy}
+              onClick={async () => {
+                // NOTE: for "insecure" non-https setup, the `navigator.clipboard.writeText` may fail
+                // as the browser may not allow the clipboard to be accessed.
+                try {
+                  const shareLink = await generateShareLink(chatSession.id);
+                  if (!shareLink) {
+                    alert("Failed to generate share link");
                   } else {
-                    alert("Failed to delete share link");
-                  }
-                }}
-                danger
-              >
-                Delete Share Link
-              </Button>
-            </div>
-          ) : (
-            <div className="flex flex-col gap-spacing-interline">
-              <Callout type="warning" title="Warning">
-                Please make sure that all content in this chat is safe to share
-                with the whole team.
-              </Callout>
-              <Button
-                leftIcon={SvgCopy}
-                onClick={async () => {
-                  // NOTE: for "insecure" non-https setup, the `navigator.clipboard.writeText` may fail
-                  // as the browser may not allow the clipboard to be accessed.
-                  try {
-                    const shareLink = await generateShareLink(chatSession.id);
-                    if (!shareLink) {
-                      alert("Failed to generate share link");
-                    } else {
-                      setShareLink(shareLink);
-                      updateCurrentChatSessionSharedStatus(
-                        ChatSessionSharedStatus.Public
-                      );
-                      copyAll(shareLink);
-                    }
-                  } catch (e) {
-                    console.error(e);
-                  }
-                }}
-                secondary
-              >
-                Generate and Copy Share Link
-              </Button>
-            </div>
-          )}
-
-          <Separator className={cn("my-4")} />
-
-          <AdvancedOptionsToggle
-            showAdvancedOptions={showAdvancedOptions}
-            setShowAdvancedOptions={setShowAdvancedOptions}
-            title="Advanced Options"
-          />
-
-          {showAdvancedOptions && (
-            <div className="flex flex-col gap-spacing-interline">
-              <Callout type="notice" title="Seed New Chat">
-                Generate a link to a new chat session with the same settings as
-                this chat (including the assistant and model).
-              </Callout>
-              <Button
-                leftIcon={SvgCopy}
-                onClick={async () => {
-                  try {
-                    const seedLink = await generateSeedLink(
-                      message,
-                      currentAgent?.id,
-                      llmManager.currentLlm
+                    setShareLink(shareLink);
+                    updateCurrentChatSessionSharedStatus(
+                      ChatSessionSharedStatus.Public
                     );
-                    if (!seedLink) {
-                      setPopup({
-                        message: "Failed to generate seed link",
-                        type: "error",
-                      });
-                    } else {
-                      navigator.clipboard.writeText(seedLink);
-                      copyAll(seedLink);
-                      setPopup({
-                        message: "Link copied to clipboard!",
-                        type: "success",
-                      });
-                    }
-                  } catch (e) {
-                    console.error(e);
-                    alert("Failed to generate or copy link.");
+                    copyAll(shareLink);
                   }
-                }}
-                secondary
-              >
-                Generate and Copy Seed Link
-              </Button>
-            </div>
-          )}
-        </ConfirmationModalContent>
-      </ModalProvider>
+                } catch (e) {
+                  console.error(e);
+                }
+              }}
+              secondary
+            >
+              Generate and Copy Share Link
+            </Button>
+          </div>
+        )}
+
+        <Separator className={cn("my-4")} />
+
+        <AdvancedOptionsToggle
+          showAdvancedOptions={showAdvancedOptions}
+          setShowAdvancedOptions={setShowAdvancedOptions}
+          title="Advanced Options"
+        />
+
+        {showAdvancedOptions && (
+          <div className="flex flex-col gap-spacing-interline">
+            <Callout type="notice" title="Seed New Chat">
+              Generate a link to a new chat session with the same settings as
+              this chat (including the assistant and model).
+            </Callout>
+            <Button
+              leftIcon={SvgCopy}
+              onClick={async () => {
+                try {
+                  const seedLink = await generateSeedLink(
+                    message,
+                    currentAgent?.id,
+                    llmManager.currentLlm
+                  );
+                  if (!seedLink) {
+                    setPopup({
+                      message: "Failed to generate seed link",
+                      type: "error",
+                    });
+                  } else {
+                    navigator.clipboard.writeText(seedLink);
+                    copyAll(seedLink);
+                    setPopup({
+                      message: "Link copied to clipboard!",
+                      type: "success",
+                    });
+                  }
+                } catch (e) {
+                  console.error(e);
+                  alert("Failed to generate or copy link.");
+                }
+              }}
+              secondary
+            >
+              Generate and Copy Seed Link
+            </Button>
+          </div>
+        )}
+      </ConfirmationModalContent>
     </>
   );
 }
