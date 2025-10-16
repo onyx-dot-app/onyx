c0272c58ac6be2414fa509f408937d1c99950389
SubashMohan <subashmohan75@gmail.com>
2025-10-16 14:20:43 +0530

    Ai Fixes

--- web/src/components/modals/AddInstructionModal.tsx
+++ web/src/components/modals/AddInstructionModal.tsx
@@ -37,8 +37,6 @@ export default function AddInstructionModal() {
     toggleModal(ModalIds.AddInstructionModal, false);
   }
 
-  useKeyPress(handleSubmit, "Enter", open);
-
   return (
     <Modal
       id={ModalIds.AddInstructionModal}
