34bb17dbda5f44a5230249ef88b72af95da90603
Raunak Bhagat <r@rabh.io>
2025-10-21 15:50:42 -0700

    Fix bug in which tooltips would be clipped

--- web/src/refresh-components/VerticalShadowScroller.tsx
+++ web/src/refresh-components/VerticalShadowScroller.tsx
@@ -7,29 +7,32 @@ export interface VerticalShadowScrollerProps {
   className?: string;
   children?: React.ReactNode;
   disable?: boolean;
+  backgroundColor?: string;
 }
 
 export default function VerticalShadowScroller({
   className,
   children,
   disable,
+  backgroundColor = "background-tint-02",
 }: VerticalShadowScrollerProps) {
   return (
-    <div
-      className={cn("flex flex-col flex-1 overflow-y-scroll", className)}
-      style={{
-        WebkitMaskImage: disable
-          ? undefined
-          : "linear-gradient(to bottom, white calc(100% - 3rem), transparent)",
-        maskImage: disable
-          ? undefined
-          : "linear-gradient(to bottom, white calc(100% - 3rem), transparent)",
-      }}
-    >
-      {children}
+    <div className="flex flex-col flex-1 overflow-y-hidden relative">
+      <div className={cn("flex flex-col flex-1 overflow-y-scroll", className)}>
+        {children}
+        {/* We add some spacing after the masked scroller to make it clear that this is the *end* of the scroller. */}
+        <div className="min-h-[0.5rem]" />
+      </div>
 
-      {/* We add some spacing after the masked scroller to make it clear that this is the *end* of the scroller. */}
-      <div className="min-h-[0.5rem]" />
+      {/* Mask Layer */}
+      <div
+        className="absolute bottom-0 left-0 right-0 h-[3rem]"
+        style={{
+          background: disable
+            ? undefined
+            : `linear-gradient(to bottom, transparent, var(--${backgroundColor}))`,
+        }}
+      />
     </div>
   );
 }
