2bb7c2542cdcd607e7f5f77bc09cffbe79733f3a
Weves <chrisweaver101@gmail.com>
2025-10-24 19:45:48 -0700

    fix typing

--- backend/onyx/db/constants.py
+++ backend/onyx/db/constants.py
@@ -6,7 +6,7 @@ CONNECTOR_VALIDATION_ERROR_MESSAGE_PREFIX = "ConnectorValidationError:"
 
 # Sentinel value to distinguish between "not provided" and "explicitly set to None"
 class UnsetType:
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "<UNSET>"
 
 
