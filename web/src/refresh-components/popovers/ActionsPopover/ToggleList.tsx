9fc06a0c5d50177a0ccfc013460990d9b1ea0951
Raunak Bhagat <r@rabh.io>
2025-10-27 16:55:16 -0700

    Make the footer be fixed

--- web/src/refresh-components/popovers/ActionsPopover/ToggleList.tsx
+++ web/src/refresh-components/popovers/ActionsPopover/ToggleList.tsx
@@ -56,7 +56,7 @@ export default function ToggleList({
   }, [items, searchTerm]);
 
   return (
-    <PopoverMenu medium>
+    <PopoverMenu medium footer={footer}>
       {[
         <div className="flex items-center gap-1" key="search">
           <IconButton
@@ -126,9 +126,6 @@ export default function ToggleList({
             </SimpleTooltip>
           );
         }),
-
-        null,
-        footer,
       ]}
     </PopoverMenu>
   );
