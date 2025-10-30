979a545a833d15e81d136e1e9aec381096b78703
Raunak Bhagat <r@rabh.io>
2025-10-29 18:01:11 -0700

    Use tailwind values instead

--- web/src/refresh-components/VerticalShadowScroller.tsx
+++ web/src/refresh-components/VerticalShadowScroller.tsx
@@ -25,7 +25,7 @@ export default function VerticalShadowScroller({
         <div style={{ minHeight }} />
       </div>
       <div
-        className="absolute bottom-[0rem] left-0 right-0 h-[3rem] z-[20] pointer-events-none"
+        className="absolute bottom-0 left-0 right-0 h-[3rem] z-[20] pointer-events-none"
         style={{
           background: disable
             ? undefined
