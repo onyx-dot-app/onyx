6663cc3e8f2bf7126991a625f9b16bf4ef5076a0
Raunak Bhagat <r@rabh.io>
2025-10-30 17:37:11 -0700

    Edit page stylings

--- web/src/refresh-components/page-components/PageHeader.tsx
+++ web/src/refresh-components/page-components/PageHeader.tsx
@@ -5,42 +5,43 @@ import { cn } from "@/lib/utils";
 import Text from "@/refresh-components/texts/Text";
 
 export interface PageHeaderProps {
+  // Header variants
+  sticky?: boolean;
+
   icon: React.FunctionComponent<SvgProps>;
   title: string;
   description: string;
+  className?: string;
   children?: React.ReactNode;
   rightChildren?: React.ReactNode;
-  sticky?: boolean;
 }
 
 export default function PageHeader({
   icon: Icon,
   title,
   description,
+  className,
   children,
   rightChildren,
   sticky = false,
 }: PageHeaderProps) {
   return (
-    <div
-      className={cn(
-        "flex flex-col gap-6 p-4"
-        // sticky && "sticky top-0 z-10"
-      )}
-    >
-      <div className="flex flex-col">
-        <div className="flex flex-row justify-between items-center gap-4">
-          <Icon className="stroke-text-04 h-[1.75rem] w-[1.75rem]" />
-          {rightChildren}
-        </div>
+    <div className={cn("pt-10", sticky && "sticky top-0 z-10", className)}>
+      <div className="flex flex-col gap-6 px-4 pt-4 pb-2">
         <div className="flex flex-col">
-          <Text headingH2>{title}</Text>
-          <Text secondaryBody text03>
-            {description}
-          </Text>
+          <div className="flex flex-row justify-between items-center gap-4">
+            <Icon className="stroke-text-04 h-[1.75rem] w-[1.75rem]" />
+            {rightChildren}
+          </div>
+          <div className="flex flex-col">
+            <Text headingH2>{title}</Text>
+            <Text secondaryBody text03>
+              {description}
+            </Text>
+          </div>
         </div>
+        <div>{children}</div>
       </div>
-      {children}
     </div>
   );
 }
--- web/src/refresh-components/page-components/PageWrapper.tsx
+++ web/src/refresh-components/page-components/PageWrapper.tsx
@@ -4,7 +4,7 @@ export default function PageWrapper(
   props: React.HtmlHTMLAttributes<HTMLDivElement>
 ) {
   return (
-    <div className="w-full h-full flex flex-col pt-10 items-center overflow-y-auto">
+    <div className="w-full h-full flex flex-col items-center overflow-y-auto">
       <div className="h-full w-[42rem]">
         <div {...props} />
       </div>
