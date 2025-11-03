0184242ad8a5af533e18cea95bc21e6b989fbb54
SubashMohan <subashmohan75@gmail.com>
2025-11-03 18:07:18 +0530

    fix ai comments

--- web/src/icons/ollama.tsx
+++ web/src/icons/ollama.tsx
@@ -6,7 +6,7 @@ const SvgOllama = (props: SVGProps<SVGSVGElement>) => (
     viewBox="0 0 16 16"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    className={`text-black dark:text-white ${props.className ?? ""}`}
+    className={`text-[var(--text-05)] ${props.className ?? ""}`}
     {...props}
   >
     <path
--- web/src/icons/openai.tsx
+++ web/src/icons/openai.tsx
@@ -8,7 +8,7 @@ const SvgOpenAI = (props: SVGProps<SVGSVGElement>) => {
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className={`text-black dark:text-white ${props.className ?? ""}`}
+      className={`text-[var(--text-05)] ${props.className ?? ""}`}
       {...props}
     >
       <g clipPath={`url(#${clipId})`}>
