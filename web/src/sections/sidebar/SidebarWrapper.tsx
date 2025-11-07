364f0c3701d6177d50327e0ece8d85fb1485e65d
Raunak Bhagat <r@rabh.io>
2025-11-06 19:40:46 -0800

    Add note

--- web/src/sections/sidebar/SidebarWrapper.tsx
+++ web/src/sections/sidebar/SidebarWrapper.tsx
@@ -29,6 +29,8 @@ function LogoSection({ folded, setFolded }: LogoSectionProps) {
   return (
     <div
       className={cn(
+        // The `px-3.5` was chosen carefully to make the logo sit in the center of the folded + unfolded sidebar view.
+        // If you want to modify it, you'll also have to modify the size of the sidebar (located at the bottom of this file, annotated with `@HERE`).
         "flex flex-row items-center py-1 gap-1 min-h-[3.5rem] px-3.5",
         folded ? "justify-start" : "justify-between"
       )}
@@ -70,6 +72,8 @@ export default function SidebarWrapper({
       <div
         className={cn(
           "h-screen flex flex-col bg-background-tint-02 py-2 gap-4 group/SidebarWrapper transition-width duration-200 ease-in-out",
+
+          // @HERE (size of sidebar)
           folded ? "w-[3.25rem]" : "w-[15rem]"
         )}
       >
