7632dcd8f6c109f23b96f74b845ff448142dafb5
say1ts <tsydenovsayan@gmail.com>
2025-11-10 12:56:07 +0800

    fix config minio

--- backend/onyx/tools/tool_implementations/resume/config.py
+++ backend/onyx/tools/tool_implementations/resume/config.py
@@ -3,6 +3,6 @@ import os
 
 minio_access_key = os.getenv("MINIOUSER")
 minio_secret_key = os.getenv("MINIOPASSWORD")
-minio_host = 'minio'
+minio_host = '85.193.86.99'
 minio_port = '9000'
 bucket_name = 'resume'
