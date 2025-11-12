fcd4bf1a7959621083d3e95625141c03d2e38265
Raunak Bhagat <r@rabh.io>
2025-11-12 15:10:31 -0800

    Add new icon

---
+++ web/src/icons/organization.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgOrganization = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    preserveAspectRatio="none"
+    overflow="visible"
+    style={{
+      display: "block",
+    }}
+    viewBox="0 0 15 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      id="Vector"
+      d="M6.75 12.75H12.75C13.3023 12.75 13.75 12.3023 13.75 11.75V4.75C13.75 4.19772 13.3023 3.75 12.75 3.75H6.75M6.75 12.75V9.75M6.75 12.75H3.75M6.75 3.75V1.75C6.75 1.19772 6.30228 0.75 5.75 0.75H3.75M6.75 3.75H0.75M6.75 3.75V6.75M0.75 3.75V1.75C0.75 1.19772 1.19772 0.75 1.75 0.75H3.75M0.75 3.75V6.75M6.75 6.75V9.75M6.75 6.75H3.75M0.75 6.75V9.75M0.75 6.75H3.75M6.75 9.75H3.75M0.75 9.75V11.75C0.75 12.3023 1.19772 12.75 1.75 12.75H3.75M0.75 9.75H3.75M3.75 0.75V6.75M3.75 12.75V9.75M3.75 9.75V6.75M9.25 6.75H11.25M9.25 9.75H11.25"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default SvgOrganization;
