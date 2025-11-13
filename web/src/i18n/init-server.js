f599f583b0f596dbec03ce0b871125bce562eae4
Vadim Severikov <camelot22rus@gmail.com>
2025-11-13 16:20:49 +0300

    Попытка фикса перевода

--- web/src/i18n/init-server.js
+++ web/src/i18n/init-server.js
@@ -3,21 +3,22 @@ import i18next from "i18next";
 const russian = require("./russian");
 const english = require("./english");
 
-i18next.init({
-  lng: "ru",
-  fallbackLng: "ru",
-  preload: ["ru", "en"],
-  resources: {
-    ru: { translations: russian },
-    en: { translations: english },
-  },
-  ns: ["translations"],
-  defaultNS: "translations",
-  interpolation: {
-    escapeValue: false,
-  },
-});
+if (!i18next.isInitialized) {
+  i18next.init({
+    lng: "ru",
+    fallbackLng: "ru",
+    preload: ["ru", "en"],
+    resources: {
+      ru: { translations: russian },
+      en: { translations: english },
+    },
+    ns: ["translations"],
+    defaultNS: "translations",
+    interpolation: {
+      escapeValue: false,
+    },
+    initImmediate: false,
+  });
+}
 
 export default i18next;
-
-
