b27c5425c678a77a0a4004ced2d445810ed547cb
Weves <chrisweaver101@gmail.com>
2025-11-14 11:07:20 -0800

    .

--- backend/alembic/versions/c7e9f4a3b2d1_add_python_tool.py
+++ backend/alembic/versions/c7e9f4a3b2d1_add_python_tool.py
@@ -1,7 +1,7 @@
 """add_python_tool
 
 Revision ID: c7e9f4a3b2d1
-Revises: a4f23d6b71c8
+Revises: 2acdef638fc2
 Create Date: 2025-11-08 00:00:00.000000
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "c7e9f4a3b2d1"
-down_revision = "a4f23d6b71c8"
+down_revision = "2acdef638fc2"
 branch_labels = None
 depends_on = None
 
