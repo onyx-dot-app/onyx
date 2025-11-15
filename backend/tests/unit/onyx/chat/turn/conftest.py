b6f05e56c642b036475cdba5564788285371371d
Richard Guan <rguan72@gmail.com>
2025-11-14 17:12:31 -0800

    .

--- backend/tests/unit/onyx/chat/turn/conftest.py
+++ backend/tests/unit/onyx/chat/turn/conftest.py
@@ -15,7 +15,6 @@ from tests.unit.onyx.chat.turn.utils import chat_turn_context
 from tests.unit.onyx.chat.turn.utils import chat_turn_dependencies
 from tests.unit.onyx.chat.turn.utils import fake_db_session
 from tests.unit.onyx.chat.turn.utils import fake_llm
-from tests.unit.onyx.chat.turn.utils import fake_model
 from tests.unit.onyx.chat.turn.utils import fake_redis_client
 from tests.unit.onyx.chat.turn.utils import fake_tools
 
@@ -100,7 +99,6 @@ __all__ = [
     "fake_db_session",
     "fake_dummy_tool",
     "fake_llm",
-    "fake_model",
     "fake_redis_client",
     "fake_tools",
 ]
