e3ae7753347021e0370198517a8e64e81e2e1184
Raunak Bhagat <r@rabh.io>
2025-11-18 19:43:24 -0800

    Render modal

--- web/src/refresh-components/layouts/layouts.tsx
+++ web/src/refresh-components/layouts/layouts.tsx
@@ -1,10 +1,11 @@
 import { ChatSession } from "@/app/chat/interfaces";
 import { cn } from "@/lib/utils";
 import Text from "@/refresh-components/texts/Text";
-import Button from "../buttons/Button";
+import Button from "@/refresh-components/buttons/Button";
 import SvgShare from "@/icons/share";
 import { CombinedSettings } from "@/app/admin/settings/interfaces";
 import { useState } from "react";
+import ShareChatSessionModal from "@/app/chat/components/modal/ShareChatSessionModal";
 
 interface AppPageProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
   settings: CombinedSettings | null;
@@ -23,27 +24,36 @@ async function AppPage({
     settings?.enterpriseSettings?.custom_header_content;
 
   return (
-    <div className="flex flex-col h-full w-full">
-      {(customHeaderContent || chatSession) && (
-        <header className="w-full flex flex-row justify-center items-center py-3 px-4 h-16 dbg-red">
-          <div className="flex-1" />
-          <div className="flex-1 flex flex-col items-center">
-            <Text text03>{customHeaderContent}</Text>
-          </div>
-          <div className="flex-1 flex flex-row items-center justify-end px-1">
-            <Button
-              leftIcon={SvgShare}
-              transient={showShareModal}
-              tertiary
-              onClick={() => setShowShareModal(true)}
-              className={cn(!chatSession && "invisible")}
-            />
-          </div>
-        </header>
+    <>
+      {showShareModal && chatSession && (
+        <ShareChatSessionModal
+          chatSession={chatSession}
+          onClose={() => setShowShareModal(false)}
+        />
       )}
 
-      <div className={cn("flex-1 overflow-auto", className)} {...rest} />
-    </div>
+      <div className="flex flex-col h-full w-full">
+        {(customHeaderContent || chatSession) && (
+          <header className="w-full flex flex-row justify-center items-center py-3 px-4 h-16 dbg-red">
+            <div className="flex-1" />
+            <div className="flex-1 flex flex-col items-center">
+              <Text text03>{customHeaderContent}</Text>
+            </div>
+            <div className="flex-1 flex flex-row items-center justify-end px-1">
+              <Button
+                leftIcon={SvgShare}
+                transient={showShareModal}
+                tertiary
+                onClick={() => setShowShareModal(true)}
+                className={cn(!chatSession && "invisible")}
+              />
+            </div>
+          </header>
+        )}
+
+        <div className={cn("flex-1 overflow-auto", className)} {...rest} />
+      </div>
+    </>
   );
 }
 
