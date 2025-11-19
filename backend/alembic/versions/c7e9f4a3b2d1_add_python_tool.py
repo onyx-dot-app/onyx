da48e1ce03598a7551c49d65d2533488c72ca539
Weves <chrisweaver101@gmail.com>
2025-11-19 13:59:24 -0800

    fix migrations

--- backend/alembic/versions/c7e9f4a3b2d1_add_python_tool.py
+++ backend/alembic/versions/c7e9f4a3b2d1_add_python_tool.py
@@ -1,7 +1,7 @@
 """add_python_tool
 
 Revision ID: c7e9f4a3b2d1
-Revises: 9drpiiw74ljy
+Revises: 1f2a3b4c5d6e
 Create Date: 2025-11-08 00:00:00.000000
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "c7e9f4a3b2d1"
-down_revision = "9drpiiw74ljy"
+down_revision = "1f2a3b4c5d6e"
 branch_labels = None
 depends_on = None
 
