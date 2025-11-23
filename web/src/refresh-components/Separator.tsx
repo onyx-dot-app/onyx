8a30bc870ff9a8f930563cf5ab335d342c8655b9
Raunak Bhagat <r@rabh.io>
2025-11-22 18:01:52 -0800

    Clean up component a bit

--- web/src/refresh-components/Separator.tsx
+++ web/src/refresh-components/Separator.tsx
@@ -4,27 +4,53 @@ import * as React from "react";
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 import { cn } from "@/lib/utils";
 
-const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-  ) => (
+/**
+ * Separator Component
+ *
+ * A visual divider that separates content either horizontally or vertically.
+ * Built on Radix UI's Separator primitive.
+ *
+ * @example
+ * ```tsx
+ * // Horizontal separator (default)
+ * <Separator />
+ *
+ * // Vertical separator
+ * <Separator orientation="vertical" />
+ *
+ * // With custom className
+ * <Separator className="my-8" />
+ *
+ * // Non-decorative (announced by screen readers)
+ * <Separator decorative={false} />
+ * ```
+ */
+function SeparatorInner(
+  {
+    className,
+    orientation = "horizontal",
+    decorative = true,
+    ...props
+  }: React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>,
+  ref: React.ForwardedRef<React.ComponentRef<typeof SeparatorPrimitive.Root>>
+) {
+  return (
     <SeparatorPrimitive.Root
       ref={ref}
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 my-4 bg-neutral-200 dark:bg-neutral-600",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        "shrink-0 border-t",
+        orientation === "horizontal"
+          ? "py-4 h-[1px] w-full"
+          : "px-4 h-full w-[1px]",
         className
       )}
       {...props}
     />
-  )
-);
-Separator.displayName = SeparatorPrimitive.Root.displayName;
+  );
+}
 
+const Separator = React.forwardRef(SeparatorInner);
+Separator.displayName = SeparatorPrimitive.Root.displayName;
 export { Separator };
