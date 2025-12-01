acfee2ac5045151d0d4c124afc7b1cf94d2315b3
Raunak Bhagat <r@rabh.io>
2025-12-01 13:20:55 -0700

    Edit + add new icons

--- web/src/icons/folder-partial-open.tsx
+++ web/src/icons/folder-partial-open.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { IconProps } from "@/icons";
+import { SvgProps } from "@/icons";
 
-const SvgFolderPartialOpen = React.forwardRef<SVGSVGElement, IconProps>(
-  ({ size = 32, color = "currentColor", title, className, ...props }, ref) => (
+const SvgFolderPartialOpen = React.forwardRef<SVGSVGElement, SvgProps>(
+  ({ size = 32, color = "currentColor", className, ...props }, ref) => (
     <svg
       ref={ref}
       xmlns="http://www.w3.org/2000/svg"
@@ -10,12 +10,9 @@ const SvgFolderPartialOpen = React.forwardRef<SVGSVGElement, IconProps>(
       width={size}
       height={size}
       fill="none"
-      role={title ? "img" : "presentation"}
-      aria-label={title}
       className={className}
       {...props}
     >
-      {title ? <title>{title}</title> : null}
       <path
         d="M14.1431 4.98782V4.25C14.1431 3.42157 13.4715 2.75 12.6431 2.75H8.76442C8.36659 2.75 7.98506 2.59196 7.70376 2.31066L6.58244 1.18934C6.30113 0.908035 5.9196 0.75 5.52178 0.75H2.6431C1.81467 0.75 1.1431 1.42157 1.1431 2.25V4.9878"
         stroke={color}
--- web/src/icons/openai.tsx
+++ web/src/icons/openai.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import type { SVGProps } from "react";
 
 const SvgOpenAI = (props: SVGProps<SVGSVGElement>) => {
