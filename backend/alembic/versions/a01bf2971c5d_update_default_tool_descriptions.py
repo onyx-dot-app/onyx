ca31b0d3b2c99fed35c053a4bdbb79e188a6d002
rohoswagger <rohod04@gmail.com>
2025-12-17 10:37:29 -0800

    migration fix

--- backend/alembic/versions/a01bf2971c5d_update_default_tool_descriptions.py
+++ backend/alembic/versions/a01bf2971c5d_update_default_tool_descriptions.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "a01bf2971c5d"
-down_revision = "87c52ec39f84"
+down_revision = "18b5b2524446"
 branch_labels = None
 depends_on = None
 
