0bec2b7b1c332e4194976505ad3d387bf3345492
Jamison Lahman <jamison@lahman.dev>
2025-12-22 00:10:26 -0800

    review

--- web/src/layouts/ChatHeader.tsx
+++ web/src/layouts/ChatHeader.tsx
@@ -183,7 +183,7 @@ export default function ChatHeader({ settings, chatSession }: ChatHeaderProps) {
       ];
       setPopoverItems(items);
     }
-  }, [showMoveOptions, filteredProjects]);
+  }, [showMoveOptions, filteredProjects, chatSession]);
 
   return (
     <>
