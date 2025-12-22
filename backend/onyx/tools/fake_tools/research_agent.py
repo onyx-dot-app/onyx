718a99d27252252fe5c22efa9520b124d9a6908e
Andrei <acaprau@gmail.com>
2025-12-22 08:12:27 -0800

    what is git?

--- backend/onyx/tools/fake_tools/research_agent.py
+++ backend/onyx/tools/fake_tools/research_agent.py
@@ -306,6 +306,7 @@ def run_research_agent_call(
                 placement=Placement(
                     turn_index=turn_index,
                     tab_index=tab_index,
+                    sub_turn_index=llm_cycle_count + reasoning_cycles,
                 ),
                 citation_processor=None,
                 state_container=None,
