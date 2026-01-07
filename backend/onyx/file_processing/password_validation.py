72c75090cd1e0b7d9e09c74302ec58f5c1ecb958
Dane Urban <durban@Danes-MacBook-Pro.local>
2026-01-06 19:24:23 -0800

    nits

--- backend/onyx/file_processing/password_validation.py
+++ backend/onyx/file_processing/password_validation.py
@@ -5,6 +5,13 @@ from typing import IO
 
 from onyx.file_processing.extract_file_text import get_file_ext
 
+PASSWORD_PROTECTED_FILES = [
+    "pdf",
+    "docx",
+    "pptx",
+    "xlsx",
+]
+
 
 def is_pdf_protected(file: IO[Any]) -> bool:
     from pypdf import PdfReader
@@ -13,20 +20,29 @@ def is_pdf_protected(file: IO[Any]) -> bool:
     return bool(reader.is_encrypted)
 
 
-def is_file_protected(
+def is_docx_protected(file: IO[Any]) -> bool: ...
+
+
+def is_pptx_protected(file: IO[Any]) -> bool: ...
+
+
+def is_xlsx_protected(file: IO[Any]) -> bool: ...
+
+
+def is_file_password_protected(
     file: IO[Any],
     file_name: str,
     extension: str | None = None,
 ) -> bool:
     extension_to_function: dict[str, Callable[[IO[Any]], str]] = {
-        ".pdf": is_pdf_protected,
+        "pdf": is_pdf_protected,
     }
 
     if not extension:
         extension = get_file_ext(file_name)
 
-    if extension in extension_to_function:
-        func = extension_to_function[extension]
-        return func(file)
+    if extension not in PASSWORD_PROTECTED_FILES:
+        return False
 
-    raise ValueError("No mechanism for permission checking type")
+    func = extension_to_function[extension]
+    return func(file)
