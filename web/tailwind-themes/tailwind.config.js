8026d9723b1eac192d18ff6c097050bc3763d491
Jamison Lahman <jamison@lahman.dev>
2026-01-09 10:49:49 -0800

    note on tailwind upgrade

--- web/tailwind-themes/tailwind.config.js
+++ web/tailwind-themes/tailwind.config.js
@@ -348,6 +348,9 @@ module.exports = {
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
     },
   ],
+  // Note: @tailwindcss/container-queries plugin is not needed here.
+  // Container queries (@container, cqw units) are native in Tailwind v4+.
+  // After upgrading to Tailwind 4, this comment can be removed.
   plugins: [
     require("@tailwindcss/typography"),
     require("@headlessui/tailwindcss"),
