8662e60ea778272da311d3eba1c9b0bdc1eaccd2
Andrei <acaprau@gmail.com>
2026-01-12 14:31:59 -0800

    ok

--- backend/onyx/document_index/opensearch/schema.py
+++ backend/onyx/document_index/opensearch/schema.py
@@ -5,6 +5,7 @@ from typing import Self
 
 from pydantic import BaseModel
 from pydantic import field_serializer
+from pydantic import field_validator
 from pydantic import model_validator
 
 from onyx.document_index.opensearch.constants import DEFAULT_MAX_CHUNK_SIZE
@@ -17,11 +18,9 @@ TITLE_FIELD_NAME = "title"
 TITLE_VECTOR_FIELD_NAME = "title_vector"
 CONTENT_FIELD_NAME = "content"
 CONTENT_VECTOR_FIELD_NAME = "content_vector"
-NUM_TOKENS_FIELD_NAME = "num_tokens"
 SOURCE_TYPE_FIELD_NAME = "source_type"
 METADATA_FIELD_NAME = "metadata"
 LAST_UPDATED_FIELD_NAME = "last_updated"
-CREATED_AT_FIELD_NAME = "created_at"
 PUBLIC_FIELD_NAME = "public"
 ACCESS_CONTROL_LIST_FIELD_NAME = "access_control_list"
 HIDDEN_FIELD_NAME = "hidden"
@@ -36,6 +35,10 @@ CHUNK_INDEX_FIELD_NAME = "chunk_index"
 MAX_CHUNK_SIZE_FIELD_NAME = "max_chunk_size"
 TENANT_ID_FIELD_NAME = "tenant_id"
 BLURB_FIELD_NAME = "blurb"
+DOC_SUMMARY_FIELD_NAME = "doc_summary"
+CHUNK_CONTEXT_FIELD_NAME = "chunk_context"
+PRIMARY_OWNERS_FIELD_NAME = "primary_owners"
+SECONDARY_OWNERS_FIELD_NAME = "secondary_owners"
 
 
 def get_opensearch_doc_chunk_id(
@@ -75,8 +78,6 @@ class DocumentChunk(BaseModel):
     title_vector: list[float] | None
     content: str
     content_vector: list[float]
-    # The actual number of tokens in the chunk.
-    num_tokens: int
 
     source_type: str
     # Contains a string representation of a dict which maps string key to either
@@ -86,7 +87,6 @@ class DocumentChunk(BaseModel):
     # of strings.
     metadata: str
     last_updated: datetime | None
-    created_at: datetime | None = None
 
     public: bool
     access_control_list: list[str]
@@ -101,19 +101,16 @@ class DocumentChunk(BaseModel):
     # chunk text to the link corresponding to that point.
     source_links: str | None
     blurb: str
+    doc_summary: str
+    chunk_context: str
 
     document_sets: list[str] | None
+    # User projects.
     project_ids: list[int] | None
+    primary_owners: list[str] | None
+    secondary_owners: list[str] | None
 
     tenant_id: str | None
-
-    @model_validator(mode="after")
-    def check_num_tokens_fits_within_max_chunk_size(self) -> Self:
-        if self.num_tokens > self.max_chunk_size:
-            raise ValueError(
-                "Bug: Num tokens must be less than or equal to max chunk size."
-            )
-        return self
 
     @model_validator(mode="after")
     def check_title_and_title_vector_are_consistent(self) -> Self:
@@ -124,7 +121,7 @@ class DocumentChunk(BaseModel):
             raise ValueError("Bug: Title must not be None if title vector is not None.")
         return self
 
-    @field_serializer("last_updated", "created_at", mode="plain")
+    @field_serializer("last_updated", mode="plain")
     def serialize_datetime_fields_to_epoch_millis(
         self, value: datetime | None
     ) -> int | None:
@@ -142,6 +139,17 @@ class DocumentChunk(BaseModel):
             value = value.astimezone(timezone.utc)
         # timestamp returns a float in seconds so convert to millis.
         return int(value.timestamp() * 1000)
+
+    @field_validator("last_updated", mode="before")
+    @classmethod
+    def parse_epoch_millis_to_datetime(cls, value: Any) -> datetime | None:
+        if value is None:
+            return None
+        if not isinstance(value, int):
+            raise ValueError(
+                f"Bug: Expected an int for the last_updated property from OpenSearch, got {type(value)} instead."
+            )
+        return datetime.fromtimestamp(value / 1000, tz=timezone.utc)
 
 
 class DocumentSchema:
@@ -217,11 +225,6 @@ class DocumentSchema:
                         "parameters": {"ef_construction": EF_CONSTRUCTION, "m": M},
                     },
                 },
-                # See TODO in _convert_onyx_chunk_to_opensearch_document. I
-                # don't want to actually add this to the schema until we know
-                # for sure we need it. If we decide we don't I will remove this.
-                # # Number of tokens in the chunk's content.
-                # NUM_TOKENS_FIELD_NAME: {"type": "integer", "store": True},
                 SOURCE_TYPE_FIELD_NAME: {"type": "keyword"},
                 METADATA_FIELD_NAME: {"type": "keyword"},
                 # TODO(andrei): Check if Vespa stores seconds, we may wanna do
@@ -233,16 +236,6 @@ class DocumentSchema:
                     # would make sense to sort by date.
                     "doc_values": True,
                 },
-                # See TODO in _convert_onyx_chunk_to_opensearch_document. I
-                # don't want to actually add this to the schema until we know
-                # for sure we need it. If we decide we don't I will remove this.
-                # CREATED_AT_FIELD_NAME: {
-                #     "type": "date",
-                #     "format": "epoch_millis",
-                #     # For some reason date defaults to False, even though it
-                #     # would make sense to sort by date.
-                #     "doc_values": True,
-                # },
                 # Access control fields.
                 # Whether the doc is public. Could have fallen under access
                 # control list but is such a broad and critical filter that it
@@ -286,9 +279,29 @@ class DocumentSchema:
                     "doc_values": False,
                     "store": False,
                 },
+                # Same as above.
+                # TODO(andrei): If we want to search on this this needs to be
+                # changed.
+                DOC_SUMMARY_FIELD_NAME: {
+                    "type": "keyword",
+                    "index": False,
+                    "doc_values": False,
+                    "store": False,
+                },
+                # Same as above.
+                # TODO(andrei): If we want to search on this this needs to be
+                # changed.
+                CHUNK_CONTEXT_FIELD_NAME: {
+                    "type": "keyword",
+                    "index": False,
+                    "doc_values": False,
+                    "store": False,
+                },
                 # Product-specific fields.
                 DOCUMENT_SETS_FIELD_NAME: {"type": "keyword"},
                 PROJECT_IDS_FIELD_NAME: {"type": "integer"},
+                PRIMARY_OWNERS_FIELD_NAME: {"type": "keyword"},
+                SECONDARY_OWNERS_FIELD_NAME: {"type": "keyword"},
                 # OpenSearch metadata fields.
                 DOCUMENT_ID_FIELD_NAME: {"type": "keyword"},
                 CHUNK_INDEX_FIELD_NAME: {"type": "integer"},
