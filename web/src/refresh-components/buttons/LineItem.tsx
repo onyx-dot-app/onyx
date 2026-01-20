e17d0c68139f189658d3b82d5885a21682fc9fb3
Raunak Bhagat <r@rabh.io>
2026-01-19 22:57:31 -0800

    Update line-item description wrapper + add size to popover in ChatInputBar

--- web/src/refresh-components/buttons/LineItem.tsx
+++ web/src/refresh-components/buttons/LineItem.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Text from "@/refresh-components/texts/Text";
 import { cn } from "@/lib/utils";
 import type { IconProps } from "@opal/types";
 import Truncated from "@/refresh-components/texts/Truncated";
@@ -150,9 +149,9 @@ export default function LineItem({
           {rightChildren && <Section alignItems="end">{rightChildren}</Section>}
         </Section>
         {description && (
-          <Text secondaryBody text03 className="text-left w-full">
+          <Truncated secondaryBody text03 className="text-left w-full">
             {description}
-          </Text>
+          </Truncated>
         )}
       </Section>
     </button>
