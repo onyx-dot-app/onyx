92181d07b2ed27e153af1b96d548436add0009e3
Raunak Bhagat <r@rabh.io>
2026-01-23 22:05:53 -0800

    fix: Fix scrollability issues for modals (#7718)

--- web/src/app/chat/components/ChatPopup.tsx
+++ web/src/app/chat/components/ChatPopup.tsx
@@ -73,7 +73,7 @@ export function ChatPopup() {
 
   return (
     <Modal open onOpenChange={() => {}}>
-      <Modal.Content tall>
+      <Modal.Content width="sm" height="lg">
         <Modal.Header
           titleClassName="text-text-04"
           icon={headerIcon}
