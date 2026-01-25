4cedcfee5998701850ff2402bc1c2cfa7974e3e2
Raunak Bhagat <r@rabh.io>
2026-01-24 17:20:26 -0800

    Fix notifications popover some more

--- web/src/sections/sidebar/Settings/Settings.tsx
+++ web/src/sections/sidebar/Settings/Settings.tsx
@@ -212,7 +212,12 @@ export default function Settings({ folded }: SettingsProps) {
             </SidebarTab>
           </div>
         </Popover.Trigger>
-        <Popover.Content align="end" side="right" fit>
+        <Popover.Content
+          align="end"
+          side="right"
+          md={popupState === "Settings"}
+          fit={popupState === "Notifications"}
+        >
           {popupState === "Settings" && (
             <SettingsPopover
               onClose={() => setPopupState(undefined)}
