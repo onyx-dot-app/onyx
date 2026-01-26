d5c30ec7548f88ac0e3ab34cc6407c50fae71194
SubashMohan <subashmohan75@gmail.com>
2026-01-26 12:45:47 +0530

    feat(chat): add tool renderers for timeline (#7567)

--- web/src/app/chat/message/messageComponents/timeline/hooks/index.ts
+++ web/src/app/chat/message/messageComponents/timeline/hooks/index.ts
@@ -6,3 +6,6 @@ export type { TimelineMetrics, UniqueTool } from "./useTimelineMetrics";
 
 export { usePacketProcessor } from "./usePacketProcessor";
 export type { UsePacketProcessorResult } from "./usePacketProcessor";
+
+export { useTimelineHeader } from "./useTimelineHeader";
+export type { TimelineHeaderResult } from "./useTimelineHeader";
