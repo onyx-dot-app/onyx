75a388194a59c3b9af53bed62e56e9a52d38c1f2
Raunak Bhagat <r@rabh.io>
2026-01-27 18:42:29 -0800

    Add layout updates

--- web/src/sections/SearchUI.tsx
+++ web/src/sections/SearchUI.tsx
@@ -239,7 +239,7 @@ export default function SearchResults({
       </div>
 
       {/* Results list */}
-      <div className="flex-1 min-h-0 overflow-y-auto py-2">
+      <div className="flex-1 min-h-0 overflow-y-auto py-4">
         <Section gap={0.5} justifyContent="start">
           {filteredAndSortedResults.map((doc) => (
             <SearchCard
