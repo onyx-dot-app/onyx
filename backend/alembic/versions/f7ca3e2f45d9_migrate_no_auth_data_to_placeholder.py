9b0dc5b158b24a6569ec297e6575833c441f515f
Jessica Singh <jessicasingh@outlook.com>
2026-01-29 11:27:06 -0800

    rev

--- backend/alembic/versions/f7ca3e2f45d9_migrate_no_auth_data_to_placeholder.py
+++ backend/alembic/versions/f7ca3e2f45d9_migrate_no_auth_data_to_placeholder.py
@@ -8,7 +8,7 @@ A database trigger is installed that automatically transfers all data from
 the placeholder user to the first real user who registers, then drops itself.
 
 Revision ID: f7ca3e2f45d9
-Revises: 849b21c732f8
+Revises: 78ebc66946a0
 Create Date: 2026-01-15 12:49:53.802741
 
 """
@@ -23,7 +23,7 @@ from shared_configs.configs import MULTI_TENANT
 
 # revision identifiers, used by Alembic.
 revision = "f7ca3e2f45d9"
-down_revision = "849b21c732f8"
+down_revision = "78ebc66946a0"
 branch_labels = None
 depends_on = None
 
