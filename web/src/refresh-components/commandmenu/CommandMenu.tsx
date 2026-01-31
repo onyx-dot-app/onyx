2416f3d1f6989543c6a6774142a2c2d29277ce14
SubashMohan <subashmohan75@gmail.com>
2026-01-31 11:08:22 +0530

    fix(ui): update CommandMenu styles for improved layout and animations

--- web/src/refresh-components/commandmenu/CommandMenu.tsx
+++ web/src/refresh-components/commandmenu/CommandMenu.tsx
@@ -350,17 +350,18 @@ const CommandMenuContent = React.forwardRef<
         ref={ref}
         onKeyDown={handleKeyDown}
         className={cn(
-          "fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
+          "fixed inset-x-0 top-[72px] mx-auto",
           "z-modal",
           "bg-background-tint-00 border rounded-16 shadow-2xl outline-none",
           "flex flex-col overflow-hidden",
-          "max-w-[calc(100dvw-2rem)] max-h-[calc(100dvh-2rem)]",
+          "max-w-[calc(100dvw-2rem)] max-h-[calc(100dvh-144px)]",
+          "data-[state=open]:animate-in data-[state=closed]:animate-out",
           "data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0",
-          "data-[state=open]:zoom-in-95 data-[state=closed]:zoom-out-95",
-          "data-[state=open]:slide-in-from-top-1/2 data-[state=closed]:slide-out-to-top-1/2",
+          "data-[state=open]:slide-in-from-bottom data-[state=open]:slide-in-from-left-0",
+          "data-[state=closed]:slide-out-to-bottom data-[state=closed]:slide-out-to-left-0",
           "duration-200",
           "w-[32rem]",
-          "min-h-[15rem] max-h-[40rem]"
+          "min-h-[15rem]"
         )}
       >
         <VisuallyHidden.Root asChild>
@@ -444,7 +445,7 @@ function CommandMenuHeader({
           onChange={(e) => onValueChange?.(e.target.value)}
           onKeyDown={handleInputKeyDown}
           autoFocus
-          className="w-full !bg-transparent !border-transparent [&:is(:hover,:active,:focus,:focus-within)]:!bg-background-neutral-00 [&:is(:hover,:active,:focus,:focus-within)]:!border-border-01 [&:is(:focus,:focus-within)]:!shadow-none"
+          className="w-full !bg-transparent !border-transparent [&:is(:hover,:active,:focus,:focus-within)]:!bg-background-neutral-00 [&:is(:hover)]:!border-border-01 [&:is(:focus,:focus-within)]:!shadow-none"
         />
       </div>
     </div>
