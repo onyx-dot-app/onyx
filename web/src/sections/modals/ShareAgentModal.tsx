620d070b6a928b8228883c23b849ac262666f979
Justin Tahara <105671973+justin-tahara@users.noreply.github.com>
2026-03-02 11:28:49 -0800

    chore(ui): Update the Share Agent Modal (#8915)

--- web/src/sections/modals/ShareAgentModal.tsx
+++ web/src/sections/modals/ShareAgentModal.tsx
@@ -251,12 +251,12 @@ function ShareAgentFormContent({ agentId }: ShareAgentFormContentProps) {
           }
           cancel={
             <Button secondary onClick={handleClose}>
-              Done
+              Cancel
             </Button>
           }
           submit={
             <Button onClick={() => handleSubmit()} disabled={!dirty}>
-              Share
+              Save
             </Button>
           }
         />
