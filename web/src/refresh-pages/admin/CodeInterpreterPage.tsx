85029f7a361c2b01c4cd0bd8b831c3bc6aa6bd0e
Dane Urban <danelegend13@gmail.com>
2026-03-02 15:12:50 -0800

    .

--- web/src/refresh-pages/admin/CodeInterpreterPage.tsx
+++ web/src/refresh-pages/admin/CodeInterpreterPage.tsx
@@ -287,7 +287,7 @@ export default function CodeInterpreterPage() {
               }
             />
           )}
-          {showErrorMenu && (
+          {showErrorMenu && !isHealthy && (
             <Section
               flexDirection="row"
               justifyContent="end"
