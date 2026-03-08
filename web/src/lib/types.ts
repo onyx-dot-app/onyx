3ca8775116513c8b0575c762fa32dcc062962879
starmovie12 <64631955+starmovie12@users.noreply.github.com>
2026-03-09 05:27:22 +0530

    Update types.ts

--- web/src/lib/types.ts
+++ web/src/lib/types.ts
@@ -474,6 +474,7 @@ export enum ValidSources {
   Outline = "outline",
   Confluence = "confluence",
   Jira = "jira",
+  JiraServiceManagement = "jira_service_management",
   Productboard = "productboard",
   Slab = "slab",
   Coda = "coda",
@@ -537,6 +538,7 @@ export const federatedSourceToRegularSource = (
 export const validAutoSyncSources = [
   ValidSources.Confluence,
   ValidSources.Jira,
+  ValidSources.JiraServiceManagement,
   ValidSources.GoogleDrive,
   ValidSources.Gmail,
   ValidSources.Slack,
