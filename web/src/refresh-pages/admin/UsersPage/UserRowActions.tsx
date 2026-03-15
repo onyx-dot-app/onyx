a94ee0796ed95c87f47faa49179a558d919e3967
Nik <nikolas.garza5@gmail.com>
2026-03-15 16:53:45 -0700

    polish: simplify inactive user actions to activate and delete only

--- web/src/refresh-pages/admin/UsersPage/UserRowActions.tsx
+++ web/src/refresh-pages/admin/UsersPage/UserRowActions.tsx
@@ -176,23 +176,6 @@ export default function UserRowActions({
       case UserStatus.INACTIVE:
         return (
           <>
-            {user.id && (
-              <Button
-                prominence="tertiary"
-                icon={SvgUsers}
-                onClick={() => openModal(Modal.EDIT_GROUPS)}
-              >
-                Groups &amp; Roles
-              </Button>
-            )}
-            <Button
-              prominence="tertiary"
-              icon={SvgKey}
-              onClick={() => openModal(Modal.RESET_PASSWORD)}
-            >
-              Reset Password
-            </Button>
-            <Separator paddingXRem={0.5} />
             <Button
               prominence="tertiary"
               icon={SvgUserPlus}
@@ -200,6 +183,7 @@ export default function UserRowActions({
             >
               Activate User
             </Button>
+            <Separator paddingXRem={0.5} />
             <Button
               prominence="tertiary"
               variant="danger"
