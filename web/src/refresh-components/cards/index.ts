1678ce73fa3259d4ffcdf22aeb93d24c76f942d0
Raunak Bhagat <r@rabh.io>
2026-03-17 09:04:52 -0700

    Remove `NameCard`

--- web/src/refresh-components/cards/index.ts
+++ web/src/refresh-components/cards/index.ts
@@ -1,6 +1,4 @@
 export { default as Card } from "./Card";
 export type { CardProps } from "./Card";
-export { default as NameCard } from "./NameCard";
-export type { NameCardProps } from "./NameCard";
 export { default as Select } from "./Select";
 export type { SelectProps } from "./Select";
