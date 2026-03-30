917814d2411993e1fc90d8da7c3254a03128c76e
justin-tahara <justintahara@gmail.com>
2026-03-30 16:30:22 -0700

    Address comment

--- deployment/terraform/modules/aws/onyx/variables.tf
+++ deployment/terraform/modules/aws/onyx/variables.tf
@@ -275,4 +275,9 @@ variable "eks_cloudwatch_log_group_retention_in_days" {
   type        = number
   description = "Number of days to retain EKS control plane logs in CloudWatch (0 = never expire)"
   default     = 30
+
+  validation {
+    condition     = contains([0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1096, 1827, 2192, 2557, 2922, 3288, 3653], var.eks_cloudwatch_log_group_retention_in_days)
+    error_message = "Must be a valid CloudWatch retention value (0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1096, 1827, 2192, 2557, 2922, 3288, 3653)."
+  }
 }
