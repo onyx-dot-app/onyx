e45d0317f7a5454db6ebecb52c6048d110085194
Raunak Bhagat <r@rabh.io>
2026-04-13 10:09:24 -0700

    Remove `nonInteractive` from invocations

--- web/src/refresh-pages/admin/GroupsPage/EditGroupPage.tsx
+++ web/src/refresh-pages/admin/GroupsPage/EditGroupPage.tsx
@@ -450,7 +450,6 @@ function EditGroupPage({ groupId }: EditGroupPageProps) {
                   title="Delete This Group"
                   description="Members will lose access to any resources shared with this group."
                   center
-                  nonInteractive
                 >
                   <Button
                     variant="danger"
