0c60257edc46d36f8682a07e223b36a9fb1659c9
Dane Urban <danelegend13@gmail.com>
2026-05-05 21:18:58 -0700

    Admin / Action changes

--- web/src/app/app/components/tools/constants.ts
+++ web/src/app/app/components/tools/constants.ts
@@ -1,5 +1,12 @@
 import type { IconProps } from "@opal/types";
-import { SvgCode, SvgGlobe, SvgImage, SvgLink, SvgSearch } from "@opal/icons";
+import {
+  SvgCode,
+  SvgGlobe,
+  SvgImage,
+  SvgLink,
+  SvgSearch,
+  SvgTerminal,
+} from "@opal/icons";
 
 // Tool names as referenced by tool results / tool calls
 export const SEARCH_TOOL_NAME = "run_search";
@@ -15,6 +22,7 @@ export const WEB_SEARCH_TOOL_ID = "WebSearchTool";
 export const PYTHON_TOOL_ID = "PythonTool";
 export const OPEN_URL_TOOL_ID = "OpenURLTool";
 export const FILE_READER_TOOL_ID = "FileReaderTool";
+export const CODING_AGENT_TOOL_ID = "CodingAgentTool";
 
 // Icon mappings for system tools
 export const SYSTEM_TOOL_ICONS: Record<
@@ -26,4 +34,5 @@ export const SYSTEM_TOOL_ICONS: Record<
   [IMAGE_GENERATION_TOOL_ID]: SvgImage,
   [PYTHON_TOOL_ID]: SvgCode,
   [OPEN_URL_TOOL_ID]: SvgLink,
+  [CODING_AGENT_TOOL_ID]: SvgTerminal,
 };
