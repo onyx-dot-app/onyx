8a04643b2b9dabc10aaf90cad3246d40a6365021
Bo-Onyx <bo@onyx.app>
2026-05-19 12:42:56 -0700

    fix(alembic): repoint content_hash migration onto 9618d5038140

--- backend/alembic/versions/ea418a384b9d_add_document_content_hash.py
+++ backend/alembic/versions/ea418a384b9d_add_document_content_hash.py
@@ -1,7 +1,7 @@
 """add content_hash to document
 
 Revision ID: ea418a384b9d
-Revises: e4ed20ddae7c
+Revises: 9618d5038140
 Create Date: 2026-05-18 12:00:00.000000
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "ea418a384b9d"
-down_revision = "e4ed20ddae7c"
+down_revision = "9618d5038140"
 branch_labels = None
 depends_on = None
 
