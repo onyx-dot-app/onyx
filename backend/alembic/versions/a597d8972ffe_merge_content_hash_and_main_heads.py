f358a7911c757ec82b1bab3ea537b8bb20aa8d91
Bo-Onyx <bo@onyx.app>
2026-05-19 12:35:16 -0700

    fix(alembic): merge content_hash and main migration heads

---
+++ backend/alembic/versions/a597d8972ffe_merge_content_hash_and_main_heads.py
@@ -0,0 +1,21 @@
+"""merge content_hash and main heads
+
+Revision ID: a597d8972ffe
+Revises: 9618d5038140, ea418a384b9d
+Create Date: 2026-05-19 12:34:59.067629
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "a597d8972ffe"
+down_revision = ("9618d5038140", "ea418a384b9d")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
