john-office/danswer

commit 20394db3793e6f836b9fc5b527a935a3d673178e

refactor(icons): migrate legacy icons/logos to opal (#10474)

web/src/app/admin/bots/[bot-id]/SlackChannelConfigsTable.tsx
@@ -2,7 +2,7 @@
 
 import { PageSelector } from "@/components/PageSelector";
 import { toast } from "@/hooks/useToast";
-import { EditIcon } from "@/components/icons/icons";
+import { SvgEdit } from "@opal/icons";
 import { SlackChannelConfig } from "@/lib/types";
 import {
   Table,
@@ -84,7 +84,7 @@ export default function SlackChannelConfigsTable({
                       <TableCell>
                         <div className="flex gap-x-2">
                           <div className="my-auto">
-                            <EditIcon className="text-muted-foreground" />
+                            <SvgEdit className="text-muted-foreground" />
                           </div>
                           <div className="my-auto">
                             {"#" +

web/src/components/credentials/actions/ModifyCredential.tsx
@@ -3,7 +3,7 @@ import Modal from "@/refresh-components/Modal";
 import Text from "@/refresh-components/texts/Text";
 import { Badge } from "@/components/ui/badge";
 import { AccessType } from "@/lib/types";
-import { EditIcon, NewChatIcon, SwapIcon } from "@/components/icons/icons";
+import { SvgEdit } from "@opal/icons";
 import {
   ConfluenceCredentialJson,
   Credential,
@@ -132,7 +132,7 @@ function CredentialSelectionTable({
                         onClick={() => onEditCredential(credential)}
                         className="cursor-pointer my-auto"
                       >
-                        <EditIcon />
+                        <SvgEdit />
                       </button>
                     )}
                   </td>

web/lib/opal/src/icons/book.tsx
@@ -1,0 +1,20 @@
+import type { IconProps } from "@opal/types";
+const SvgBook = ({ size, ...props }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke="currentColor"
+    {...props}
+  >
+    <path
+      d="M2.66666 13V2.99998C2.66666 2.0922 3.42555 1.33331 4.33332 1.33331H13.3333V14.6666H4.33332C3.42554 14.6666 2.66666 13.9078 2.66666 13ZM2.66666 13C2.66666 12.0922 3.42555 11.3333 4.33332 11.3333H13.3333"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default SvgBook;

web/lib/opal/src/icons/index.ts
@@ -23,6 +23,7 @@ export { default as SvgBarChart } from "@opal/icons/bar-chart";
 export { default as SvgBarChartSmall } from "@opal/icons/bar-chart-small";
 export { default as SvgBell } from "@opal/icons/bell";
 export { default as SvgBlocks } from "@opal/icons/blocks";
+export { default as SvgBook } from "@opal/icons/book";
 export { default as SvgBookOpen } from "@opal/icons/book-open";
 export { default as SvgBookmark } from "@opal/icons/bookmark";
 export { default as SvgBooksLineSmall } from "@opal/icons/books-line-small";

web/lib/opal/src/logos/brave.tsx
@@ -1,0 +1,60 @@
+import type { IconProps } from "@opal/types";
+const SvgBrave = ({ size, ...props }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 218 256"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <defs>
+      <linearGradient
+        x1="0%"
+        y1="50.7055163%"
+        x2="100%"
+        y2="50.7055163%"
+        id="brave-grad1"
+      >
+        <stop stopColor="#FF5500" offset="0%" />
+        <stop stopColor="#FF5500" offset="40.9877232%" />
+        <stop stopColor="#FF2000" offset="58.1981215%" />
+        <stop stopColor="#FF2000" offset="100%" />
+      </linearGradient>
+      <linearGradient
+        x1="2.1484375%"
+        y1="50.7055163%"
+        x2="100%"
+        y2="50.7055163%"
+        id="brave-grad2"
+      >
+        <stop stopColor="#FF452A" offset="0%" />
+        <stop stopColor="#FF2000" offset="100%" />
+      </linearGradient>
+      <path
+        d="M170.272109,25.3359387 L147.968109,0.00010893617 L108.800109,0.00010893617 L69.6321088,0.00010893617 L47.3281088,25.3359387 C47.3281088,25.3359387 27.7441088,19.8891302 18.4961088,29.1487047 C18.4961088,29.1487047 44.6081088,26.7886026 53.5841088,41.4040238 C53.5841088,41.4040238 77.7921088,46.0338111 81.0561088,46.0338111 C84.3201088,46.0338111 91.3921088,43.3104068 97.9201088,41.1316834 C104.448109,38.95296 108.800109,38.9371643 108.800109,38.9371643 C108.800109,38.9371643 113.152109,38.95296 119.680109,41.1316834 C126.208109,43.3104068 133.280109,46.0338111 136.544109,46.0338111 C139.808109,46.0338111 164.016109,41.4040238 164.016109,41.4040238 C172.992109,26.7886026 199.104109,29.1487047 199.104109,29.1487047 C189.856109,19.8891302 170.272109,25.3359387 170.272109,25.3359387"
+        id="brave-path"
+      />
+    </defs>
+    <g stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+      <g transform="translate(-70.000000, -350.000000)">
+        <g transform="translate(50.000000, 350.000000)">
+          <g transform="translate(20.114286, 0.000000)">
+            <path
+              d="M209.984109,61.2848749 L215.968109,46.5784919 C215.968109,46.5784919 208.352109,38.4082791 199.104109,29.1487047 C189.856109,19.8891302 170.272109,25.3359387 170.272109,25.3359387 L147.968109,0.00010893617 L108.800109,0.00010893617 L69.6321088,0.00010893617 L47.3281088,25.3359387 C47.3281088,25.3359387 27.7441088,19.8891302 18.4961088,29.1487047 C9.2481088,38.4082791 1.6321088,46.5784919 1.6321088,46.5784919 L7.6161088,61.2848749 L0.0001088,83.0721089 C0.0001088,83.0721089 22.3993088,168.017811 25.0241088,178.391258 C30.1921088,198.81679 33.7281088,206.714662 48.4161088,217.063598 C63.1041088,227.412534 89.7601088,245.387003 94.1121088,248.110407 C98.4641088,250.833811 103.904109,255.472858 108.800109,255.472858 C113.696109,255.472858 119.136109,250.833811 123.488109,248.110407 C127.840109,245.387003 154.496109,227.412534 169.184109,217.063598 C183.872109,206.714662 187.408109,198.81679 192.576109,178.391258 C195.200365,168.017811 217.600109,83.0721089 217.600109,83.0721089 L209.984109,61.2848749 Z"
+              fill="url(#brave-grad1)"
+            />
+            <path
+              d="M164.016109,41.4040238 C164.016109,41.4040238 192.704493,76.1274281 192.704493,83.5487047 C192.704493,90.9699813 189.095597,92.9286536 185.467117,96.7866281 C181.838637,100.644603 165.991373,117.49376 163.956269,119.657232 C161.921165,121.821249 157.684493,125.101862 160.176557,131.006747 C162.668621,136.911632 166.344973,144.425505 162.256813,152.046679 C158.168109,159.667854 151.164109,164.754628 146.676109,163.913641 C142.188109,163.072109 131.648109,157.557215 127.772109,155.038066 C123.896109,152.518917 111.611501,142.374781 111.611501,138.493386 C111.611501,134.612534 124.310093,127.643888 126.655821,126.0605 C129.002637,124.477658 139.703117,118.349454 139.922349,115.944143 C140.141037,113.538288 140.057805,112.832926 136.899885,106.889369 C133.741965,100.945266 128.054989,93.0136238 129.001549,87.7362111 C129.948109,82.459343 139.119949,79.7152409 145.665357,77.2402111 C152.209677,74.765726 164.811437,70.0918196 166.385229,69.3652153 C167.960109,68.6380664 167.553197,67.945777 162.783949,67.4931472 C158.015789,67.0405174 144.483245,65.2419813 138.382285,66.9446536 C132.281325,68.647326 121.858285,71.2378281 121.013997,72.6115132 C120.169709,73.9851983 119.424973,74.0314962 120.292109,78.7702196 C121.158701,83.508943 125.622765,106.24719 126.055789,110.285998 C126.489357,114.324807 127.336909,116.994832 122.987629,117.990509 C118.637805,118.986186 111.316109,120.715003 108.800109,120.715003 C106.284109,120.715003 98.9618688,118.986186 94.6125888,117.990509 C90.2627648,116.994832 91.1103168,114.324807 91.5438848,110.285998 C91.9774528,106.24719 96.4409728,83.508943 97.3081088,78.7702196 C98.1747008,74.0314962 97.4299648,73.9851983 96.5862208,72.6115132 C95.7419328,71.2378281 85.3183488,68.647326 79.2173888,66.9446536 C73.1164288,65.2419813 59.5844288,67.0405174 54.8157248,67.4931472 C50.0470208,67.945777 49.6401088,68.6380664 51.2144448,69.3652153 C52.7887808,70.0918196 65.3905408,74.765726 71.9348608,77.2402111 C78.4797248,79.7152409 87.6521088,82.459343 88.5986688,87.7362111 C89.5452288,93.0136238 83.8577088,100.945266 80.7003328,106.889369 C77.5424128,112.832926 77.4586368,113.538288 77.6778688,115.944143 C77.8965568,118.349454 88.5975808,124.477658 90.9438528,126.0605 C93.2901248,127.643888 105.988173,134.612534 105.988173,138.493386 C105.988173,142.374781 93.7041088,152.518917 89.8281088,155.038066 C85.9521088,157.557215 75.4121088,163.072109 70.9241088,163.913641 C66.4361088,164.754628 59.4321088,159.667854 55.3434048,152.046679 C51.2552448,144.425505 54.9315968,136.911632 57.4231168,131.006747 C59.9151808,125.101862 55.6790528,121.821249 53.6434048,119.657232 C51.6088448,117.49376 35.7610368,100.644603 32.1325568,96.7866281 C28.5040768,92.9286536 24.8957248,90.9699813 24.8957248,83.5487047 C24.8957248,76.1274281 53.5841088,41.4040238 53.5841088,41.4040238 C53.5841088,41.4040238 77.7921088,46.0338111 81.0561088,46.0338111 C84.3201088,46.0338111 91.3921088,43.3104068 97.9201088,41.1316834 C104.448109,38.95296 108.800109,38.9371643 108.800109,38.9371643 C108.800109,38.9371643 113.152109,38.95296 119.680109,41.1316834 C126.208109,43.3104068 133.280109,46.0338111 136.544109,46.0338111 C139.808109,46.0338111 164.016109,41.4040238 164.016109,41.4040238 Z M142.509504,174.227935 C144.28512,175.341263 143.202016,177.439918 141.584704,178.584837 C139.966848,179.729757 118.228064,196.584361 116.118432,198.447169 C114.008256,200.310523 110.908,203.387425 108.8,203.387425 C106.692,203.387425 103.5912,200.310523 101.481568,198.447169 C99.371392,196.584361 77.633152,179.729757 76.015296,178.584837 C74.39744,177.439918 73.31488,175.341263 75.090496,174.227935 C76.866656,173.115152 82.422528,170.306233 90.08912,166.333876 C97.754624,162.362063 107.308896,158.985042 108.8,158.985042 C110.291104,158.985042 119.844832,162.362063 127.511424,166.333876 C135.177472,170.306233 140.733344,173.115152 142.509504,174.227935 Z"
+              fill="#FFFFFF"
+            />
+            <mask id="brave-mask" fill="white">
+              <use href="#brave-path" />
+            </mask>
+            <use fill="url(#brave-grad2)" href="#brave-path" />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default SvgBrave;

web/lib/opal/src/logos/eleven-labs.tsx
@@ -1,0 +1,15 @@
+import type { IconProps } from "@opal/types";
+const SvgElevenLabs = ({ size, ...props }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M10.5 2H13V14H10.5V2Z" fill="var(--text-05)" />
+    <path d="M3 2H5.5V14H3V2Z" fill="var(--text-05)" />
+  </svg>
+);
+export default SvgElevenLabs;

web/lib/opal/src/logos/exa.tsx
@@ -1,0 +1,19 @@
+import type { IconProps } from "@opal/types";
+const SvgExa = ({ size, ...props }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 52 52"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9.58008 5H42.4164V8.13433L28.403 26L42.4164 43.8657V47H9.58008V5ZM26.2047 23.1092L37.5922 8.13433H14.8173L26.2047 23.1092ZM13.2744 11.887V24.4329H22.8147L13.2744 11.887ZM22.8147 27.5671H13.2744V40.113L22.8147 27.5671ZM14.8173 43.8657L26.2047 28.8908L37.5922 43.8657H14.8173Z"
+      fill="#1F40ED"
+    />
+  </svg>
+);
+export default SvgExa;

web/lib/opal/src/logos/firecrawl.tsx
@@ -1,0 +1,17 @@
+import type { IconProps } from "@opal/types";
+const SvgFirecrawl = ({ size, ...props }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 52 52"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M37.3021 17.3417C35.4346 17.896 34.0268 19.15 32.9958 20.5122C32.7744 20.8046 32.313 20.5849 32.4009 20.2265C34.3749 12.1093 31.7671 5.36271 23.6379 2.04179C23.2256 1.87279 22.7963 2.24291 22.9045 2.67556C26.6023 17.5225 11.0487 16.2702 13.0143 33.1013C13.0481 33.3902 12.7236 33.588 12.487 33.419C11.7501 32.89 10.9271 31.7864 10.3626 31.0107C10.1969 30.7826 9.83866 30.8468 9.7626 31.1188C9.31304 32.7447 9.1001 34.2758 9.1001 35.7969C9.1001 41.712 12.1405 46.919 16.7425 49.9374C17.0062 50.1098 17.3442 49.863 17.2546 49.5605C17.018 48.7662 16.8845 47.928 16.8743 47.061C16.8743 46.5286 16.9081 45.9844 16.991 45.4774C17.1836 44.2031 17.6265 42.9897 18.37 41.8844C20.9203 38.0565 26.0327 34.3586 25.2165 29.3376C25.1641 29.0198 25.5393 28.8103 25.7759 29.0283C29.3774 32.3188 30.0906 36.745 29.4991 40.7149C29.4484 41.0596 29.8811 41.2439 30.0991 40.9734C30.65 40.2839 31.3227 39.6789 32.0544 39.2243C32.237 39.111 32.4804 39.1972 32.5497 39.4C32.9569 40.5848 33.562 41.6968 34.1332 42.8089C34.816 44.1456 35.1794 45.6718 35.1219 47.2875C35.0932 48.0733 34.963 48.8338 34.745 49.5571C34.6521 49.863 34.9867 50.1182 35.2554 49.9425C39.8608 46.9241 42.9029 41.7171 42.9029 35.7986C42.9029 33.7418 42.5429 31.7256 41.8619 29.8361C40.4338 25.873 36.8103 22.8968 37.7263 17.7321C37.7702 17.4853 37.5421 17.2707 37.3021 17.3417Z"
+      fill="#FA5D19"
+    />
+  </svg>
+);
+export default SvgFirecrawl;

onyx-dot-app/onyx

commit 44ab3e9affc9f480c566368198cc3715c819b2f8

fix(icons): size remaining unsized @opal/icons usages
Same regression as the SlackBotTable fix earlier in this PR (introduced by
#10474 icon migration). @opal/icons require explicit size prop; tags rendered
without size + without Tailwind w-N/h-N fall back to intrinsic SVG sizing.

- ModifyCredential.tsx: Edit credential button (size=16)
- SlackChannelConfigsTable.tsx: channel-row edit hint (size=16)
- LicenseActivationCard.tsx: SvgXCircle was wrapped in a 12x12 div but the
  SVG itself was unsized and overflowed (size=12)

web/src/app/admin/billing/LicenseActivationCard.tsx
@@ -202,7 +202,7 @@ export default function LicenseActivationCard({
                 height="auto"
               >
                 <div className="billing-error-icon">
-                  <SvgXCircle />
+                  <SvgXCircle size={12} />
                 </div>
                 <Text secondaryBody text04>
                   {error}.{" "}

web/src/app/admin/bots/[bot-id]/SlackChannelConfigsTable.tsx
@@ -87,7 +87,10 @@ export default function SlackChannelConfigsTable({
                       <TableCell>
                         <div className="flex gap-x-2">
                           <div className="my-auto">
-                            <SvgEdit className="text-muted-foreground" />
+                            <SvgEdit
+                              size={16}
+                              className="text-muted-foreground"
+                            />
                           </div>
                           <div className="my-auto">
                             {"#" +

web/src/components/credentials/actions/ModifyCredential.tsx
@@ -133,7 +133,7 @@ function CredentialSelectionTable({
                         className="cursor-pointer my-auto"
                         aria-label="Edit credential"
                       >
-                        <SvgEdit />
+                        <SvgEdit size={16} />
                       </button>
                     )}
                   </td>