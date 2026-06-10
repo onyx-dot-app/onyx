d05c515e1f63a33be017f6736753f2b85abbbf06
Raunak Bhagat <r@rabh.io>
2026-06-09 18:52:01 -0700

    fix(opal): use currentColor stroke on onyx-logo icon

--- web/lib/opal/src/icons/onyx-logo.tsx
+++ web/lib/opal/src/icons/onyx-logo.tsx
@@ -5,13 +5,13 @@ const SvgOnyxLogo = ({ size, ...props }: IconProps) => (
     height={size}
     viewBox="0 0 16 16"
     fill="none"
+    stroke="currentColor"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <g clipPath="url(#clip0_586_577)">
       <path
         d="M8 4.00001L4.5 2.50002L8 1.00002L11.5 2.50002L8 4.00001Z"
-        stroke="black"
         strokeOpacity={0.75}
         strokeWidth={1.5}
         strokeLinecap="round"
@@ -19,7 +19,6 @@ const SvgOnyxLogo = ({ size, ...props }: IconProps) => (
       />
       <path
         d="M8 12L11.5 13.5L8 15L4.5 13.5L8 12Z"
-        stroke="black"
         strokeOpacity={0.75}
         strokeWidth={1.5}
         strokeLinecap="round"
@@ -27,7 +26,6 @@ const SvgOnyxLogo = ({ size, ...props }: IconProps) => (
       />
       <path
         d="M4 8L2.5 11.5L1 8L2.5 4.50002L4 8Z"
-        stroke="black"
         strokeOpacity={0.75}
         strokeWidth={1.5}
         strokeLinecap="round"
@@ -35,7 +33,6 @@ const SvgOnyxLogo = ({ size, ...props }: IconProps) => (
       />
       <path
         d="M12 8.00002L13.5 4.50002L15 8.00001L13.5 11.5L12 8.00002Z"
-        stroke="black"
         strokeOpacity={0.75}
         strokeWidth={1.5}
         strokeLinecap="round"
