a5a2774109da0219bd029a5b2131baab3fe18fd0
Raunak Bhagat <r@rabh.io>
2026-06-22 13:50:37 -0700

    Remove dead import

--- web/src/app/admin/billing/BillingDetailsView.tsx
+++ web/src/app/admin/billing/BillingDetailsView.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
 import { markdown } from "@opal/utils";
 import { Section } from "@/layouts/general-layouts";
 import { Content, InputErrorText, InputVertical } from "@opal/layouts";
