f23542f759d2abcdb37351d785ac633003d5b889
Raunak Bhagat <r@rabh.io>
2026-09-23 12:13:49 -0700

    refactor(connectors): the add-connector page names its props and uses an absolute import

--- web/src/views/admin/connectors/AddConnectorPage/index.tsx
+++ web/src/views/admin/connectors/AddConnectorPage/index.tsx
@@ -1,9 +1,11 @@
 import { ConfigurableSources } from "@/lib/types";
-import ConnectorWrapper from "./ConnectorWrapper";
+import ConnectorWrapper from "@/views/admin/connectors/AddConnectorPage/ConnectorWrapper";
 
-export default async function Page(props: {
+export interface PageProps {
   params: Promise<{ connector: string }>;
-}) {
+}
+
+export default async function Page(props: PageProps) {
   const params = await props.params;
   return (
     <ConnectorWrapper
